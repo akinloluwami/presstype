7fd68a6d470abf111b09e23755a0a2dfd48ab415
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-23 20:48:28 +0100

    wrapped with theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ThemeContextProvider } from "@/contexts/ThemeProvider";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeContextProvider>
+      <Component {...pageProps} />
+    </ThemeContextProvider>
+  );
 }
