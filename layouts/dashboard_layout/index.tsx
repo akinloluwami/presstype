83b3ea0655cba0e3623fdf1e9da329b4eb34e285
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-23 20:48:03 +0100

    ___

--- layouts/dashboard_layout/index.tsx
+++ layouts/dashboard_layout/index.tsx
@@ -1,10 +1,13 @@
 import Sidebar from "@/components/dashboard/sidebar";
 import React from "react";
+import styles from "./styles.module.scss";
 
 const DashboardLayout = ({ children }: any) => {
   return (
-    <div>
-      <Sidebar />
+    <div className={styles.dashboard_layout_container}>
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
       <>{children}</>
     </div>
   );
