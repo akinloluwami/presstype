9e5e781e5979ea7dadaaacc55f0cc21379259afb
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-23 21:55:26 +0100

    use theme

--- layouts/dashboard_layout/index.tsx
+++ layouts/dashboard_layout/index.tsx
@@ -1,11 +1,13 @@
 import Sidebar from "@/components/dashboard/sidebar";
+import { useTheme } from "@/contexts/ThemeContext";
 import React from "react";
 import styles from "./styles.module.scss";
 
 const DashboardLayout = ({ children }: any) => {
+  const { theme } = useTheme();
   return (
-    <div className={styles.dashboard_layout_container}>
-      <div className={styles.sidebar}>
+    <div className={styles.dashboard_layout_container} data-theme={theme}>
+      <div className={styles.sidebar} data-theme={theme}>
         <Sidebar />
       </div>
       <>{children}</>
