0f96ec005c6c867a27fee5e940c3a5f81451a46a
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 00:28:56 +0100

    routes array

--- components/dashboard/sidebar/index.tsx
+++ components/dashboard/sidebar/index.tsx
@@ -1,11 +1,43 @@
 import { useTheme } from "@/contexts/ThemeContext";
 import React from "react";
+import styles from "./styles.module.scss";
 
 const Sidebar = () => {
   const { theme, toggleTheme } = useTheme();
+  const routes = [
+    {
+      title: "Dahboard",
+      path: "/",
+      icon: "",
+    },
+    {
+      title: "Posts",
+      path: "/posts",
+      icon: "",
+    },
+    {
+      title: "Analytics",
+      path: "/analytics",
+      icon: "",
+    },
+    {
+      title: "Appearance",
+      path: "/appearance",
+      icon: "",
+    },
+    {
+      title: "Domain",
+      path: "/domain",
+      icon: "",
+    },
+    {
+      title: "Settings",
+      path: "/settingss",
+      icon: "",
+    },
+  ];
   return (
     <div>
-      Sidebar
       <button onClick={toggleTheme}>
         Toggle to {theme === "light" ? "dark" : "light"} mode
       </button>
