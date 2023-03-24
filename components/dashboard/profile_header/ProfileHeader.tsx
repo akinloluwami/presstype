9f392be898f660af44dd6edce2744a9749308ec4
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 11:05:26 +0100

    added content

--- components/dashboard/profile_header/ProfileHeader.tsx
+++ components/dashboard/profile_header/ProfileHeader.tsx
@@ -1,7 +1,18 @@
+import { useTheme } from "@/contexts/ThemeContext";
 import React from "react";
+import styles from "./styles.module.scss";
 
 const ProfileHeader = () => {
-  return <div>ProfileHeader</div>;
+  const { theme } = useTheme();
+  return (
+    <div className={styles.profile_header} data-theme={theme}>
+      <h3>Hi, Akinkunmi</h3>
+      <img
+        src="https://api.dicebear.com/5.x/adventurer-neutral/svg?seed=Willow"
+        alt="avatar"
+      />
+    </div>
+  );
 };
 
 export default ProfileHeader;
