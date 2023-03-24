af62eeefaba67620a34d49893f076eafe164521e
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 23:50:02 +0100

    ___

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,13 @@
 import NoPost from "@/components/dashboard/no_post/NoPost";
+import Posts from "@/components/dashboard/posts/Posts";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const Index = () => {
   return (
     <DashboardLayout page_name="Posts">
-      <NoPost />
+      {/* <NoPost /> */}
+      <Posts />
     </DashboardLayout>
   );
 };
