4309e32d226d794f56f15ab56b914f4a992d9b1f
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 11:27:20 +0100

    added stats data

--- components/dashboard/stats/index.tsx
+++ components/dashboard/stats/index.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 import styles from "./styles.module.scss";
 const Stats = () => {
+  const statsData = [
+    {
+      number: 1320000,
+      text: "Page views",
+      icon: "",
+    },
+    {
+      number: 72492,
+      text: "Subcribers",
+      icon: "",
+    },
+    {
+      number: 969,
+      text: "Posts",
+      icon: "",
+    },
+    {
+      number: 972345,
+      text: "Visitors",
+      icon: "",
+    },
+  ];
   return <div className={styles.stats_container}>Index</div>;
 };
 
