99319d5ec2fece741c173c3140768eb340f068c6
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 10:33:26 +0100

    imported search bar

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,15 @@
+import Searchbar from "@/components/dashboard/searchbar";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const DashboardHome = () => {
-  return <DashboardLayout>DashboardHome</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <div className="">
+        <Searchbar />
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default DashboardHome;
