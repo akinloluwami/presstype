f832dee0f9308d9462c76cc76b804d1151240d8c
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 23:38:17 +0100

    added empty box image

--- components/dashboard/no_post/NoPost.tsx
+++ components/dashboard/no_post/NoPost.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { ImFileEmpty } from "react-icons/im";
+import { RiCheckboxIndeterminateLine } from "react-icons/ri";
 import { RxRocket } from "react-icons/rx";
 import styles from "./styles.module.scss";
 
 const NoPost = () => {
   return (
-    <div>
-      <h1>
-        <ImFileEmpty />
-      </h1>
+    <div className={styles.no_post_container}>
+      <Image
+        src={"/images/box_empty.png"}
+        width={190}
+        height={130}
+        alt={"Empty box"}
+      />
       <p>You don't have any post yet.</p>
       <Link href="/">
         <RxRocket />
