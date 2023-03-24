5a2b32e6f6b338fa635a284d11c9a0bd26f33d7b
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 10:37:24 +0100

    imported profile header component

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,12 +1,15 @@
+import ProfileHeader from "@/components/dashboard/profile_header/ProfileHeader";
 import Searchbar from "@/components/dashboard/searchbar";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
+import styles from "./styles.module.scss";
 
 const DashboardHome = () => {
   return (
     <DashboardLayout>
-      <div className="">
+      <div className={styles.dashboard_top}>
         <Searchbar />
+        <ProfileHeader />
       </div>
     </DashboardLayout>
   );
