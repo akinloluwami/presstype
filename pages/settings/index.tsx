77f9cb2409458d8f52e3cf4dc139f4007412b7e9
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 02:00:55 +0100

    created new pages

---
+++ pages/settings/index.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/dashboard_layout";
+import React from "react";
+
+const Index = () => {
+  return <DashboardLayout></DashboardLayout>;
+};
+
+export default Index;
