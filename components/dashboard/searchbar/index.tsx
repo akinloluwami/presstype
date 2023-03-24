179fa0e4106d5eba42e9d89e10dd955edd8b5837
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 10:49:18 +0100

    added data-theme attr

--- components/dashboard/searchbar/index.tsx
+++ components/dashboard/searchbar/index.tsx
@@ -1,12 +1,15 @@
+import { useTheme } from "@/contexts/ThemeContext";
 import React from "react";
 import styles from "./styles.module.scss";
 
 const Searchbar = () => {
+  const { theme } = useTheme();
   return (
     <input
       type="text"
       placeholder="Search..."
       className={styles.searchbar}
+      data-theme={theme}
     ></input>
   );
 };
