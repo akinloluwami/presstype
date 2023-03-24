f47a0f62a7309ead6c7ef2abb179527a85fcd335
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 22:36:02 +0100

    added content

--- components/dashboard/no_post/NoPost.tsx
+++ components/dashboard/no_post/NoPost.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
-
+import { ImFileEmpty } from "react-icons/im";
+import { RxRocket } from "react-icons/rx";
 const NoPost = () => {
-  return <div>NoPost</div>;
+  return (
+    <div>
+      <h1>
+        <ImFileEmpty />
+      </h1>
+      <p>You don't have any post yet.</p>
+      <Link href="/">
+        <RxRocket />
+        Publish your first post
+      </Link>
+    </div>
+  );
 };
 
 export default NoPost;
