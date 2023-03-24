87412ccef6662705798a0323ff2eb9b3261c479d
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 22:39:08 +0100

    imported style file

--- components/dashboard/no_post/NoPost.tsx
+++ components/dashboard/no_post/NoPost.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { ImFileEmpty } from "react-icons/im";
 import { RxRocket } from "react-icons/rx";
+import styles from "./styles.module.scss";
+
 const NoPost = () => {
   return (
     <div>
