82019703c05f4d343fae3f1680e41f22f6105e57
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-24 22:37:54 +0100

    added no post component

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,13 @@
+import NoPost from "@/components/dashboard/no_post/NoPost";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const Index = () => {
-  return <DashboardLayout page_name="Posts"></DashboardLayout>;
+  return (
+    <DashboardLayout page_name="Posts">
+      <NoPost />
+    </DashboardLayout>
+  );
 };
 
 export default Index;
