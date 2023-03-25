27c4b0a11794bf2e74565b6012e71846c05f267a
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 19:36:06 +0100

    added publish button

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -2,7 +2,13 @@ import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const NewPost = () => {
-  return <DashboardLayout page_name={"Add new post"}></DashboardLayout>;
+  return (
+    <DashboardLayout
+      page_name={"Add new post"}
+      showButton={true}
+      button={<button>Publish</button>}
+    ></DashboardLayout>
+  );
 };
 
 export default NewPost;
