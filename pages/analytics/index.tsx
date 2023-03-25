f4f582b3d90586b75230b6c4ed66a87437d4596b
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 22:48:42 +0100

    ___

--- pages/analytics/index.tsx
+++ pages/analytics/index.tsx
@@ -2,14 +2,25 @@ import BarChart from "@/components/dashboard/barchart/BarChart";
 import LineChart from "@/components/dashboard/linechart/LineChart";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React, { useState } from "react";
+import styles from "./styles.module.scss";
 
 const Index = () => {
   const [graphType, setGraphType] = useState("Discrete");
   return (
     <DashboardLayout page_name={"Analytics"}>
-      <div className="">
-        <button onClick={() => setGraphType("Discrete")}>Discrete</button>
-        <button onClick={() => setGraphType("Cummulative")}>Cummulative</button>
+      <div className={styles.buttons}>
+        <button
+          className={`${graphType === "Discrete" && styles.active}`}
+          onClick={() => setGraphType("Discrete")}
+        >
+          Discrete
+        </button>
+        <button
+          className={`${graphType === "Cummulative" && styles.active}`}
+          onClick={() => setGraphType("Cummulative")}
+        >
+          Cummulative
+        </button>
       </div>
       {graphType === "Discrete" && <BarChart />}
       {graphType === "Cummulative" && <LineChart />}
