9b168279cabfa972b0afe8bc8690d428e11dece9
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 19:11:36 +0100

    created new post page

---
+++ pages/posts/new/index.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/dashboard_layout";
+import React from "react";
+
+const NewPost = () => {
+  return <DashboardLayout>NewPost</DashboardLayout>;
+};
+
+export default NewPost;
