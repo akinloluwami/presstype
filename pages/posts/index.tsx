ce6581297a59d8ea330ef4b7d8a822ae56ae771b
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 18:52:26 +0100

    added button

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,16 @@
 import NoPost from "@/components/dashboard/no_post/NoPost";
 import Posts from "@/components/dashboard/posts/Posts";
 import DashboardLayout from "@/layouts/dashboard_layout";
+import Link from "next/link";
 import React from "react";
 
 const Index = () => {
   return (
-    <DashboardLayout page_name="Posts">
+    <DashboardLayout
+      page_name="Posts"
+      showButton={true}
+      button={<Link href={"/new-post"}>Add new</Link>}
+    >
       {/* <NoPost /> */}
       <Posts />
     </DashboardLayout>
