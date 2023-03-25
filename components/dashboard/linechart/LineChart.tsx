c765fee4c693c0e66ff00ac754752d3c57a4c84e
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 22:00:27 +0100

    created linechart component

---
+++ components/dashboard/linechart/LineChart.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
+
+const LineChart = () => {
+  return (
+    <div>
+      <Line options={options} data={data} />
+    </div>
+  );
+};
+
+export default LineChart;
--- pages/analytics/index.tsx
+++ pages/analytics/index.tsx
@@ -1,8 +1,15 @@
+import BarChart from "@/components/dashboard/barchart/BarChart";
+import LineChart from "@/components/dashboard/linechart/LineChart";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const Index = () => {
-  return <DashboardLayout page_name={"Analytics"}></DashboardLayout>;
+  return (
+    <DashboardLayout page_name={"Analytics"}>
+      <BarChart />
+      <LineChart />
+    </DashboardLayout>
+  );
 };
 
 export default Index;
