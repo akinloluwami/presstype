143939a85cd10d93a6efc1e849c4e5324d950857
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 19:27:37 +0100

    added page title

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -2,7 +2,7 @@ import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const NewPost = () => {
-  return <DashboardLayout>NewPost</DashboardLayout>;
+  return <DashboardLayout page_name={"Add new post"}></DashboardLayout>;
 };
 
 export default NewPost;
