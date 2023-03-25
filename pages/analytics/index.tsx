5ae715a9b74c1276df8ecd17a2e03e80e301eb70
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-25 22:36:58 +0100

    ___

--- pages/analytics/index.tsx
+++ pages/analytics/index.tsx
@@ -1,13 +1,18 @@
 import BarChart from "@/components/dashboard/barchart/BarChart";
 import LineChart from "@/components/dashboard/linechart/LineChart";
 import DashboardLayout from "@/layouts/dashboard_layout";
-import React from "react";
+import React, { useState } from "react";
 
 const Index = () => {
+  const [graphType, setGraphType] = useState("Discrete");
   return (
     <DashboardLayout page_name={"Analytics"}>
-      <BarChart />
-      <LineChart />
+      <div className="">
+        <button onClick={() => setGraphType("Discrete")}>Discrete</button>
+        <button onClick={() => setGraphType("Cummulative")}>Cummulative</button>
+      </div>
+      {graphType === "Discrete" && <BarChart />}
+      {graphType === "Cummulative" && <LineChart />}
     </DashboardLayout>
   );
 };
