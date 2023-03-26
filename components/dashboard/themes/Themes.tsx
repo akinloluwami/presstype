1d2e15fc1fd0c8ee80b4e59b374c98d21ecc48b8
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-26 22:24:59 +0100

    ___

--- components/dashboard/themes/Themes.tsx
+++ components/dashboard/themes/Themes.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 
 const Themes = () => {
+  const [selected, setSelected] = useState(0);
+  const themes = [0, 1, 2];
   return (
     <div className={styles.themes_container}>
       <h3>Theme</h3>
       <div className={styles.theme_cards}>
-        <div className={styles.theme_card}></div>
-        <div className={styles.theme_card}></div>
-        <div className={styles.theme_card}></div>
+        {themes.map((theme, i) => (
+          <div
+            className={`${styles.theme_card} ${
+              selected === theme && styles.active
+            }`}
+            onClick={() => setSelected(theme)}
+          ></div>
+        ))}
       </div>
     </div>
   );
