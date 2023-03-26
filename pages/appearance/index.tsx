0bd3672f320210edff7c9a510f87abd9ab94d0b4
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-26 22:11:45 +0100

    imported themes component

--- pages/appearance/index.tsx
+++ pages/appearance/index.tsx
@@ -1,8 +1,13 @@
+import Themes from "@/components/dashboard/themes/Themes";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
 const Index = () => {
-  return <DashboardLayout page_name="Appearance"></DashboardLayout>;
+  return (
+    <DashboardLayout page_name="Appearance">
+      <Themes />
+    </DashboardLayout>
+  );
 };
 
 export default Index;
