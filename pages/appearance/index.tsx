bfd61dac420bfa2757ac71700a1ca7906ea2d064
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-26 23:43:12 +0100

    added upload blog logo component

--- pages/appearance/index.tsx
+++ pages/appearance/index.tsx
@@ -1,4 +1,5 @@
 import Themes from "@/components/dashboard/themes/Themes";
+import UploadBlogLogo from "@/components/dashboard/upload_blog_logo/UploadBlogLogo";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 
@@ -6,6 +7,7 @@ const Index = () => {
   return (
     <DashboardLayout page_name="Appearance">
       <Themes />
+      <UploadBlogLogo />
     </DashboardLayout>
   );
 };
