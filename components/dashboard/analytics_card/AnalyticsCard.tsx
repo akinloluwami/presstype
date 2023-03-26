d1241a589caa1067aef5dfce85ffa78586429937
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-26 11:41:08 +0100

    Analytics card

--- components/dashboard/analytics_card/AnalyticsCard.tsx
+++ components/dashboard/analytics_card/AnalyticsCard.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const AnalyticsCard = () => {
-  return <div>AnalyticsCard</div>;
+const AnalyticsCard = ({
+  title,
+  data,
+}: {
+  title: string;
+  data: { text: string; number: number }[];
+}) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className="">
+        {data.map((d, i) => (
+          <div className="" key={i}>
+            <p>{d.text}</p>
+            <p>{d.number}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AnalyticsCard;
