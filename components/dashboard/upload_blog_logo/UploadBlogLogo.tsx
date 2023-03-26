bb6581b265dd5905e0a289c0161ea18f60976683
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-26 23:43:21 +0100

    structure

--- components/dashboard/upload_blog_logo/UploadBlogLogo.tsx
+++ components/dashboard/upload_blog_logo/UploadBlogLogo.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BiCloudUpload } from "react-icons/bi";
 
 const UploadBlogLogo = () => {
-  return <div>UploadBlogLogo</div>;
+  return (
+    <>
+      <button>
+        <h2>
+          <BiCloudUpload />
+        </h2>
+        <p>Upload</p>
+      </button>
+    </>
+  );
 };
 
 export default UploadBlogLogo;
