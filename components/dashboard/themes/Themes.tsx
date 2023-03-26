cb989b7937b8fd8f45593acb6c7230b189cf59bc
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-26 22:03:33 +0100

    themes container

--- components/dashboard/themes/Themes.tsx
+++ components/dashboard/themes/Themes.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const Themes = () => {
-  return <div>Themes</div>;
+  return (
+    <div className={styles.themes_container}>
+      <h3>Themes</h3>
+      <div className={styles.theme_cards}>
+        <div className={styles.theme_card}></div>
+        <div className={styles.theme_card}></div>
+        <div className={styles.theme_card}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Themes;
