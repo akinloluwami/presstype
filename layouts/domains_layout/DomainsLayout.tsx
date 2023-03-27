9f4871be0c50a9758c55fb5bb86e90d96d0d7dd4
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 22:03:56 +0100

    added links

--- layouts/domains_layout/DomainsLayout.tsx
+++ layouts/domains_layout/DomainsLayout.tsx
@@ -2,6 +2,16 @@ import { useRouter } from "next/router";
 import React from "react";
 
 const DomainsLayout = ({ children }: any) => {
+  const links = [
+    {
+      title: "presstype.co sub-domain",
+      route: "/domain",
+    },
+    {
+      title: "Custom Domain",
+      route: "/domain/custom",
+    },
+  ];
   const router = useRouter();
   return (
     <div>
