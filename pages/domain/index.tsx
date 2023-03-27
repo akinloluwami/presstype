313f74a7eb7c545b4d5a018dda5ed789f24d67e0
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 22:07:51 +0100

    layout

--- pages/domain/index.tsx
+++ pages/domain/index.tsx
@@ -1,8 +1,13 @@
 import DashboardLayout from "@/layouts/dashboard_layout";
+import DomainsLayout from "@/layouts/domains_layout/DomainsLayout";
 import React from "react";
 
 const Index = () => {
-  return <DashboardLayout page_name="Domain"></DashboardLayout>;
+  return (
+    <DashboardLayout page_name="Domain">
+      <DomainsLayout>Subdomain</DomainsLayout>
+    </DashboardLayout>
+  );
 };
 
 export default Index;
