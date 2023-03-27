c491c4c5012491568b936ac6428855c7533e9a46
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 22:46:26 +0100

    InputGroup

--- components/elements/input_group/InputGroup.tsx
+++ components/elements/input_group/InputGroup.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const InputGroup = ({ text }: { text: string }) => {
   return (
-    <div>
-      <input type="text" />
+    <div className={styles.input_group_container}>
+      <input type="text" placeholder="sub-domain" />
       {text}
     </div>
   );
