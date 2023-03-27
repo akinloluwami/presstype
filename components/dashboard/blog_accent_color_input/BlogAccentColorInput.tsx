106227bdceb7ad85f116a65d47866fbdacffed8f
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 21:15:09 +0100

    structure

--- components/dashboard/blog_accent_color_input/BlogAccentColorInput.tsx
+++ components/dashboard/blog_accent_color_input/BlogAccentColorInput.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const BlogAccentColorInput = () => {
-  return <div>BlogAccentColorInput</div>;
+  return (
+    <div className={styles.container}>
+      <h3>Blog Accent Color</h3>
+      <input placeholder="#573AD8" type={"text"} />
+    </div>
+  );
 };
 
 export default BlogAccentColorInput;
