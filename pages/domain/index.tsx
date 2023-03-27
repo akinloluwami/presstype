be71ad2744ae7739a9353dff651588ba3a516dc4
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 22:41:21 +0100

    input group import

--- pages/domain/index.tsx
+++ pages/domain/index.tsx
@@ -1,9 +1,14 @@
+import InputGroup from "@/components/elements/input_group/InputGroup";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import DomainsLayout from "@/layouts/domains_layout/DomainsLayout";
 import React from "react";
 
 const Index = () => {
-  return <DomainsLayout>Subdomain</DomainsLayout>;
+  return (
+    <DomainsLayout>
+      <InputGroup text="presstype.co" />
+    </DomainsLayout>
+  );
 };
 
 export default Index;
