75e49f27dbb6c83a6d8242535120cafcfb352b6c
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 22:50:47 +0100

    data-theme

--- components/elements/input_group/InputGroup.tsx
+++ components/elements/input_group/InputGroup.tsx
@@ -1,10 +1,15 @@
+import { useTheme } from "@/contexts/ThemeContext";
 import React from "react";
 import styles from "./styles.module.scss";
 
 const InputGroup = ({ text }: { text: string }) => {
   return (
     <div className={styles.input_group_container}>
-      <input type="text" placeholder="sub-domain" />
+      <input
+        type="text"
+        placeholder="sub-domain"
+        data-theme={useTheme().theme}
+      />
       {text}
     </div>
   );
