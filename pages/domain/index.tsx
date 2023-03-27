49cc1f88dd05a2ad651a84a7c1698fee2b2031dc
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-27 22:57:10 +0100

    ____

--- pages/domain/index.tsx
+++ pages/domain/index.tsx
@@ -6,6 +6,20 @@ import React from "react";
 const Index = () => {
   return (
     <DomainsLayout>
+      <h4
+        style={{
+          fontWeight: 500,
+        }}
+      >
+        Change presstype.co sub-domain
+      </h4>
+      <i
+        style={{
+          fontSize: 10,
+        }}
+      >
+        Presstype won't redirect your old sub-domain to the new one.
+      </i>
       <InputGroup text=".presstype.co" />
     </DomainsLayout>
   );
