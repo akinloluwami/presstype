919ef7792edb3c26c7f408d40f8b688b8b62f0d5
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-29 22:42:44 +0100

    added data

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,10 +3,32 @@ import ProfileHeader from "@/components/dashboard/profile_header/ProfileHeader";
 import Searchbar from "@/components/dashboard/searchbar";
 import Stats from "@/components/dashboard/stats";
 import DashboardLayout from "@/layouts/dashboard_layout";
+import { faker } from "@faker-js/faker";
 import React from "react";
 import styles from "./styles.module.scss";
 
 const DashboardHome = () => {
+  const labels = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sarturday",
+  ];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Page views",
+        data: labels.map(() => faker.datatype.number({ min: 500, max: 1000 })),
+        fill: true,
+        backgroundColor: "rgba(87, 58, 216, 0.5)",
+        tension: 0.3,
+      },
+    ],
+  };
   return (
     <DashboardLayout page_name={"Dashboard"}>
       <div className={styles.dashboard_top}>
@@ -14,7 +36,7 @@ const DashboardHome = () => {
         <ProfileHeader /> */}
       </div>
       <Stats />
-      <BarChart />
+      <BarChart data={data} />
     </DashboardLayout>
   );
 };
