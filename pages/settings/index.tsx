c32ac1462a84567b4a75ef3adf0bb1a3cd19ae8e
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-29 23:04:40 +0100

    added classname and data theme

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -7,7 +7,21 @@ const Index = () => {
   const { theme } = useTheme();
   return (
     <DashboardLayout page_name="Settings">
-      <div className={styles.settings_card}></div>
+      <div className={styles.settings_card}>
+        <h4>Publication settings</h4>
+        <div className={styles.settings_card_container} data-theme={theme}>
+          <div className={styles.settings_card_container_header}>
+            <div className="">
+              <h5>Title & Description</h5>
+              <small>
+                The details used to identify your publication around the web
+              </small>
+            </div>
+            <button>Expand</button>
+          </div>
+          <input type="text" />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
