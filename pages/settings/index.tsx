5bdbce290377558f7c9e559fbb98ac6c03c90692
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-29 22:53:23 +0100

    imported theme context

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,8 +1,10 @@
+import { useTheme } from "@/contexts/ThemeContext";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 import styles from "./styles/module.scss";
 
 const Index = () => {
+  const { theme } = useTheme();
   return <DashboardLayout page_name="Settings"></DashboardLayout>;
 };
 
