83da161d83cd16e82db7011e39256046a8486f21
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-29 22:55:19 +0100

    fixed typo

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,11 +1,15 @@
 import { useTheme } from "@/contexts/ThemeContext";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
-import styles from "./styles/module.scss";
+import styles from "./styles.module.scss";
 
 const Index = () => {
   const { theme } = useTheme();
-  return <DashboardLayout page_name="Settings"></DashboardLayout>;
+  return (
+    <DashboardLayout page_name="Settings">
+      <div className={styles.settings_card}></div>
+    </DashboardLayout>
+  );
 };
 
 export default Index;
