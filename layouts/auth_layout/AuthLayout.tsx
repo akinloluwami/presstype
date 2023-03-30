aa653f542d30c06179209ae6fac2cc64def6f564
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-30 10:08:26 +0100

    interface for authlayout

--- layouts/auth_layout/AuthLayout.tsx
+++ layouts/auth_layout/AuthLayout.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
-const AuthLayout = ({ children }: any) => {
+interface AuthLayoutProps {
+  children: any;
+  auth_type: "Sign in" | "Sign up";
+}
+
+const AuthLayout = ({ children, auth_type }: AuthLayoutProps) => {
   return (
     <div className={styles.auth_layout_container}>
       <h1>P</h1>
       <h2>Welcome to PressType</h2>
-      <div className="">{children}</div>
+
+      <div className="">
+        <p>{auth_type} with one of the following:</p>
+        {children}
+      </div>
     </div>
   );
 };
