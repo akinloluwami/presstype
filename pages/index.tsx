0964fcdf46b6f1d4ced1f74a220a22956b56c13b
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-30 10:31:34 +0100

    added option to sign in with magic link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import AuthButton from "@/components/auth/auth_button/AuthButton";
 import AuthLayout from "@/layouts/auth_layout/AuthLayout";
 import React from "react";
 import { SiApple, SiGithub, SiGoogle } from "react-icons/si";
-
+import { GiFairyWand } from "react-icons/gi";
 const Home = () => {
   function log() {
     console.log("Hii");
   }
   return (
     <AuthLayout auth_type={"Sign in"}>
+      <AuthButton action={log} bg_color="#D1D5DA">
+        <GiFairyWand /> Magic Link
+      </AuthButton>
       <AuthButton action={log} bg_color="#D1D5DA">
         <SiGoogle /> Google
       </AuthButton>
