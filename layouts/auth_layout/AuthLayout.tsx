4f3823a976a9e57025e3283df9850d5c9cc4f267
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-30 09:38:37 +0100

    added content and divisions

--- layouts/auth_layout/AuthLayout.tsx
+++ layouts/auth_layout/AuthLayout.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const AuthLayout = ({ children }: any) => {
-  return <div className={styles.auth_layout_container}>{children}</div>;
+  return (
+    <div className={styles.auth_layout_container}>
+      <h1>P</h1>
+      <h2>Welcome to PressType</h2>
+      <div className="">{children}</div>
+    </div>
+  );
 };
 
 export default AuthLayout;
