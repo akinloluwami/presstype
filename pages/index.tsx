24793f08b9e5523e4c0f373c41eed2418e228248
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-30 09:39:15 +0100

    ___

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import AuthLayout from "@/layouts/auth_layout/AuthLayout";
 import React from "react";
 
 const Home = () => {
-  return <AuthLayout>Home</AuthLayout>;
+  return <AuthLayout></AuthLayout>;
 };
 
 export default Home;
