95c0cb8bfbfdd7614b48e5def7ead11b3d57e62e
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-30 09:48:06 +0100

    imported auth button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
+import AuthButton from "@/components/auth/auth_button/AuthButton";
 import AuthLayout from "@/layouts/auth_layout/AuthLayout";
 import React from "react";
 
 const Home = () => {
-  return <AuthLayout></AuthLayout>;
+  function log() {
+    console.log("Hii");
+  }
+  return (
+    <AuthLayout>
+      <AuthButton action={log} bg_color="red">
+        GitHub
+      </AuthButton>
+    </AuthLayout>
+  );
 };
 
 export default Home;
