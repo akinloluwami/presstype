e6fc5098e4a07bf80bf33dd3319224838b8ba7b3
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-31 02:10:15 +0100

    @types

--- components/auth/magic_link/MagicLink.tsx
+++ components/auth/magic_link/MagicLink.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
-const MagicLink = () => {
-  return <div className={styles.magic_link_container}></div>;
+interface MagicLinkProps {
+  type: "Sign in" | "Sign up";
+}
+
+const MagicLink = ({ type }: MagicLinkProps) => {
+  return (
+    <div className={styles.magic_link_container}>
+      <h1>P</h1>
+      <div className="">
+        <input placeholder="Email address" />
+        <button></button>
+      </div>
+    </div>
+  );
 };
 
 export default MagicLink;
