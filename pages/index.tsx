a4c62f60e0d1f4a49b57887e969da7de3fcb9fba
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-01 22:29:54 +0100

    magic link

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,19 @@ import AuthLayout from "@/layouts/auth_layout/AuthLayout";
 import React from "react";
 import { SiApple, SiGithub, SiGoogle } from "react-icons/si";
 import { GiFairyWand } from "react-icons/gi";
+import { useRouter } from "next/router";
 const Home = () => {
   function log() {
     console.log("Hii");
   }
+
+  const router = useRouter();
+  const magicLink = () => {
+    router.push("/signin/magic-link");
+  };
   return (
     <AuthLayout auth_type={"Sign in"}>
-      <AuthButton action={log} bg_color="#D1D5DA">
+      <AuthButton action={magicLink} bg_color="#D1D5DA">
         <GiFairyWand /> Magic Link
       </AuthButton>
       <AuthButton action={log} bg_color="#D1D5DA">
