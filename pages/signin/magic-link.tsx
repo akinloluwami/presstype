13e3c438fb1150b7cf50f353f3aae2a262ed1e0a
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-01 15:03:02 +0100

    sign in with magic link page

---
+++ pages/signin/magic-link.tsx
@@ -0,0 +1,12 @@
+import MagicLink from "@/components/auth/magic_link/MagicLink";
+import React from "react";
+
+const MagicLinkSignin = () => {
+  return (
+    <>
+      <MagicLink type="Sign in" />
+    </>
+  );
+};
+
+export default MagicLinkSignin;
