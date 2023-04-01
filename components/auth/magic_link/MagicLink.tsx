7ca8de2de18a047517e37514e40c31fd01c58feb
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-01 14:36:24 +0100

    added theme__

--- components/auth/magic_link/MagicLink.tsx
+++ components/auth/magic_link/MagicLink.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { useTheme } from "@/contexts/ThemeContext";
 
 interface MagicLinkProps {
   type: "Sign in" | "Sign up";
 }
 
 const MagicLink = ({ type }: MagicLinkProps) => {
+  const { theme } = useTheme();
+
   return (
     <div className={styles.magic_link_container}>
       <h1>P</h1>
