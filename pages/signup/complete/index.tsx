d49b16acacaadb62612d5ccbe08eebad78024973
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-01 22:41:14 +0100

    structure

--- pages/signup/complete/index.tsx
+++ pages/signup/complete/index.tsx
@@ -1,7 +1,21 @@
+import DoYouWantToImport from "@/components/auth/signup_steps/DoYouWantToImport";
+import PubNameAndDesc from "@/components/auth/signup_steps/PubNameAndDesc";
 import React from "react";
 
 const Complete = () => {
-  return <div>Complete</div>;
+  return (
+    <div>
+      Complete
+      <div className="">
+        <PubNameAndDesc />
+        <DoYouWantToImport />
+      </div>
+      <div className="">
+        <button>Back</button>
+        <button>Continue</button>
+      </div>
+    </div>
+  );
 };
 
 export default Complete;
