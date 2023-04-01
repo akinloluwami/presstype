314a30dba226d6e30044a6d57f5432c90e372c55
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-01 22:44:44 +0100

    next and prev handler

--- pages/signup/complete/index.tsx
+++ pages/signup/complete/index.tsx
@@ -4,12 +4,19 @@ import { useState } from "react";
 
 const Complete = () => {
   const [step, setStep] = useState<number>(0);
+
+  const nextHandler = () => {
+    setStep(step + 1);
+  };
+  const prevHandler = () => {
+    setStep(step - 1);
+  };
   return (
     <div>
       Complete
       <div className="">
-        <PubNameAndDesc />
-        <DoYouWantToImport />
+        {step === 0 && <PubNameAndDesc />}
+        {step === 1 && <DoYouWantToImport />}
       </div>
       <div className="">
         <button>Back</button>
