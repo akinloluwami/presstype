c603dbf295f53dbc6df649ddf8e79d6da3fc132c
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-01 22:36:28 +0100

    2 steps

---
+++ components/auth/signup_steps/DoYouWantToImport.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DoYouWantToImport = () => {
+  return <div>DoYouWantToImport</div>;
+};
+
+export default DoYouWantToImport;
---
+++ components/auth/signup_steps/PubNameAndDesc.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PubNameAndDesc = () => {
+  return <div>PubNameAndDesc</div>;
+};
+
+export default PubNameAndDesc;
