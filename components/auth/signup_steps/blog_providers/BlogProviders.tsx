74a5ca5960ef4ecf3d8dfe4913edb1c22d17463b
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 10:26:44 +0100

    provider arr

--- components/auth/signup_steps/blog_providers/BlogProviders.tsx
+++ components/auth/signup_steps/blog_providers/BlogProviders.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { SiBlogger, SiHashnode, SiMedium, SiWordpress } from "react-icons/si";
 
 const BlogProviders = () => {
+  const providers = [
+    {
+      name: "Wordpress",
+      logo: <SiWordpress />,
+    },
+    {
+      name: "Medium",
+      logo: <SiMedium />,
+    },
+    {
+      name: "Hashnode",
+      logo: <SiHashnode />,
+    },
+    {
+      name: "Blogger",
+      logo: <SiBlogger />,
+    },
+  ];
   return (
     <div>
       <h2>Choose your provider</h2>
+      <div className={styles.providers}></div>
     </div>
   );
 };
