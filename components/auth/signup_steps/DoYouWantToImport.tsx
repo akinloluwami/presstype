5efafa9157515e0fc0b2d5a67aed9370d67c0822
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 10:02:06 +0100

    logic wan kee me

--- components/auth/signup_steps/DoYouWantToImport.tsx
+++ components/auth/signup_steps/DoYouWantToImport.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const DoYouWantToImport = () => {
-  return <div>DoYouWantToImport</div>;
+  const [yes, setYes] = useState(false);
+  const [wannaImport, setWannaImport] = useState(false);
+  const [chosen, setChosen] = useState(false);
+  const [chosen2, setChosen2] = useState(false);
+  return (
+    <div>
+      {!chosen && (
+        <div className="">
+          <p>Do you have an existing blog?</p>
+          <button
+            onClick={() => {
+              setYes(true);
+              setChosen(true);
+            }}
+          >
+            Yes
+          </button>
+          <Link href={"/dashboard"}>
+            <button>No, continue to dashboard</button>
+          </Link>
+        </div>
+      )}
+      {yes && !chosen2 && (
+        <div className="">
+          <p>Do you wanna import your posts?</p>
+          <button
+            onClick={() => {
+              setWannaImport(true);
+              setChosen2(true);
+            }}
+          >
+            Yes
+          </button>
+          <Link href={"/dashboard"}>
+            <button>No, continue to dashboard</button>
+          </Link>
+        </div>
+      )}
+
+      {wannaImport && (
+        <div className="">
+          <h4>Choose your provider</h4>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default DoYouWantToImport;
