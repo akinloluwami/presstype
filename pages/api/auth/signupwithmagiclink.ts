90242c15de006e7ad73f6ee05912a7580b42fada
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 22:00:59 +0100

    let's not even get into the weeds

--- pages/api/auth/signupwithmagiclink.ts
+++ pages/api/auth/signupwithmagiclink.ts
@@ -37,12 +37,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const magicLinkUrl = `${process.env.BASE_URL}/signup/complete?token=${token}&email=${email}`;
     const to = email;
     const subject = "Welcome to PressType";
-    const text = "Welcome to PressType";
     const html = `
-      <h1>Welcome to PressType</h1>
-      <p>Click the link below to verify your email and complete your signup</p>
-      <a href={http://${magicLinkUrl}}> ${magicLinkUrl} </a>
+    <h1>Welcome to PressType</h1>
+    <p>Click the link below to verify your email and complete your signup</p>
+    <a href="http://${magicLinkUrl}" target="_blank" style="background-color: #007bff; color: #fff; padding: 12px 24px; border-radius: 4px; text-decoration: none;">Verify email</a>
+    
       `;
+    sendMail(to, subject, html);
     res
       .status(201)
       .send("Sign up successful, check your email to complete your signup.");
