35d7817b64bb8c5b329530f593bc46b787e8129a
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 08:52:29 +0100

    _

--- components/auth/signup_steps/PubNameAndDesc.tsx
+++ components/auth/signup_steps/PubNameAndDesc.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const PubNameAndDesc = () => {
-  return <div>PubNameAndDesc</div>;
+  return (
+    <div className={styles.pub_name_n_desc}>
+      <div className={styles.pub_name_n_bc}>
+        <div className="">
+          <p>Publication name</p>
+          <input type="text" />
+        </div>
+        {/* <div className="">
+          <p>Brand color</p>
+          <input type="color" />
+        </div> */}
+      </div>
+      <div className={styles.pub_about}>
+        <p>What's your publication about?</p>
+        <textarea placeholder="Describe your publication in 200 characters or less." />
+      </div>
+    </div>
+  );
 };
 
 export default PubNameAndDesc;
