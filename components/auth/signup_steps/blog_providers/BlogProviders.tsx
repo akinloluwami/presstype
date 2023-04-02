37e6831662f54e4e5878f3bdb698205f2355bc8a
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 10:33:39 +0100

    _

--- components/auth/signup_steps/blog_providers/BlogProviders.tsx
+++ components/auth/signup_steps/blog_providers/BlogProviders.tsx
@@ -22,11 +22,11 @@ const BlogProviders = () => {
     },
   ];
   return (
-    <div>
+    <div className={styles.blog_providers}>
       <h2>Choose your provider</h2>
       <div className={styles.providers}>
         {providers.map((provider, i) => (
-          <button key={i}>
+          <button key={i} className={styles.provider}>
             {provider.logo}
             {provider.name}
           </button>
