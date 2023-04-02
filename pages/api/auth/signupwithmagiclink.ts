8a4cbfbaf997fe2c188bee3556701e8df303d1d4
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 20:22:33 +0100

    check if email exist

--- pages/api/auth/signupwithmagiclink.ts
+++ pages/api/auth/signupwithmagiclink.ts
@@ -1,4 +1,6 @@
 import { allowMethods } from "@/middlewares/allowMethods";
+import Blog from "@/schema/Blog";
+import { connectToDatabase } from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
 
@@ -10,6 +12,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
   if (!validator.isEmail(email)) {
     res.status(400).send("Invalid email");
+    return;
+  }
+
+  await connectToDatabase();
+
+  const emailExists = await Blog.findOne({ email });
+
+  if (emailExists) {
+    res.status(409).send("Email already exists");
     return;
   }
 };
