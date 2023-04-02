c253fa42de0f8bc2de077559067c0266a2b7d143
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 13:20:07 +0100

    ___

---
+++ schema/Blog.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  hasExitingBlog: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default model("Blog", blogSchema);
