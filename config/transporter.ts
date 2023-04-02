43fa022912a39c9699f956056da7c306bb037d62
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 14:12:24 +0100

    __

--- config/transporter.ts
+++ config/transporter.ts
@@ -1,13 +1,15 @@
-import nodemailer, { TransportOptions } from "nodemailer";
+import nodemailer, { Transporter } from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: Boolean(process.env.SMTP_SECURE),
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
+
+const transporter: Transporter = nodemailer.createTransport({
+  host: SMTP_HOST,
+  port: Number(SMTP_PORT),
+  secure: false,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: SMTP_USER,
+    pass: SMTP_PASS,
   },
-} as TransportOptions);
+});
 
 export default transporter;
