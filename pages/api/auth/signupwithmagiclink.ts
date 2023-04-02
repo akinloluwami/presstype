aaa26dd396a0c21ee9003a3ecb3dd9a2da0a8268
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-02 20:16:46 +0100

    types and shit

--- pages/api/auth/signupwithmagiclink.ts
+++ pages/api/auth/signupwithmagiclink.ts
@@ -1,9 +1,17 @@
 import { allowMethods } from "@/middlewares/allowMethods";
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body;
-  return res.status(200).send(email);
+  if (!email) {
+    res.status(400).send("Invalid email");
+    return;
+  }
+  if (!validator.isEmail(email)) {
+    res.status(400).send("Invalid email");
+    return;
+  }
 };
 
 export default allowMethods(["POST"])(handler);
