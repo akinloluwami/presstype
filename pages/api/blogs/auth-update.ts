c385e7a0d36deb2a942dcf75871f86484ec2cf10
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-03 23:57:38 +0100

    request token and email from query

--- pages/api/blogs/auth-update.ts
+++ pages/api/blogs/auth-update.ts
@@ -4,6 +4,8 @@ import Blog from "@/schema/Blog";
 import { connectToDatabase } from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { token, email } = req.query;
+};
 
 export default allowMethods(["POST"])(handler);
