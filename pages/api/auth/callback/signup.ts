9f8a10029ee6d7ae3335daf2aaa56a0fc17eebcb
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-03 21:16:57 +0100

    callback handler

---
+++ pages/api/auth/callback/signup.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "@/utils/db";
+import AuthToken from "@/schema/AuthToken";
+import dayjs from "dayjs";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { token, email } = req.query;
+
+  await connectToDatabase();
+
+  const authToken = await AuthToken.findOne({ token });
+
+  if (!authToken || authToken.email !== email) {
+    res.status(400).send("Invalid token or email");
+    return;
+  }
+
+  const { expiry } = authToken;
+  const now = dayjs();
+
+  if (now.isAfter(dayjs(expiry))) {
+    res.status(400).send("Token expired");
+    return;
+  }
+
+  res.redirect(
+    "/signup/complete?email=" +
+      encodeURIComponent(email) +
+      "&token=" +
+      encodeURIComponent(token as string)
+  );
+};
+
+export default handler;
