413024fe2085bad640ed646804a866794bbb5dba
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 00:16:28 +0100

    axios request

--- actions/auth/exists.ts
+++ actions/auth/exists.ts
@@ -1,3 +1,13 @@
-const blogExists = (email: string) => {};
+import axios from "axios";
+
+const blogExists = async (email: string): Promise<boolean> => {
+  try {
+    const response = await axios.get(`/api/auth/exists?email=${email}`);
+    return response.data.exists;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
 
 export default blogExists;
