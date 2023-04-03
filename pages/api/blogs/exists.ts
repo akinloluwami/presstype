589181edfc2027b4de01a7f463790c510ca6ef4e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 00:01:15 +0100

    checked if email is sent

--- pages/api/blogs/exists.ts
+++ pages/api/blogs/exists.ts
@@ -2,6 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Blog from "@/schema/Blog";
 import { allowMethods } from "@/middlewares/allowMethods";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const email = req.query.email;
+
+  if (!email) {
+    res.status(400).send("Email is required");
+    return;
+  }
+};
 
 export default allowMethods(["GET"])(handler);
