9180aea4880877b1b0e335e35ff0a06eeeb41ee4
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-03 22:08:58 +0100

    deleted

--- utils/generate_token.ts
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-
-const generateToken = () => {
-  const buffer = crypto.randomBytes(4);
-  return parseInt(buffer.toString("hex"), 16).toString(36).slice(0, 9);
-};
-
-export default generateToken;
