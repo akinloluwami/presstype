38adb9ab210b17a85c39c43b9a8af79481a301ef
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 12:19:13 +0100

    dayjs

--- pages/api/blogs/auth-update.ts
+++ pages/api/blogs/auth-update.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import DecodedToken from "@/interfaces/decodedToken";
 import decodeToken from "@/utils/decode_token";
+import dayjs from "dayjs";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.query;
@@ -22,14 +23,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const decoded: DecodedToken = decodeToken(token as string);
 
+  const now = dayjs();
+  const expirationTime = dayjs.unix(decoded.exp);
+
+  if (now.isAfter(expirationTime)) {
+    res.status(400).send("Token expired");
+    return;
+  }
   const blog = await Blog.findOne({ email: decoded.email });
 
   if (!blog) {
     res.status(404).json({ message: "Blog not found" });
     return;
   }
-
-  // const { subdomain, title, about } = req.body;
 };
 
 export default allowMethods(["POST"])(handler);
