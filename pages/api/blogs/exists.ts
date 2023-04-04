e498d26f75301a592247d16f83b8cf38ca51a821
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 11:17:07 +0100

    sent json back to client

--- pages/api/blogs/exists.ts
+++ pages/api/blogs/exists.ts
@@ -6,21 +6,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const email = req.query.email;
 
   if (!email) {
-    res.status(400).send("Email is required");
+    res.status(400).json({ message: "Email is required" });
     return;
   }
 
   try {
     const blog = await Blog.findOne({ email });
     if (!blog) {
-      res.status(404).send("Blog not found");
+      res.status(404).json({ message: "Blog not found" });
       return;
     }
 
-    res.status(200).send("Blog found");
+    res.status(200).json({ message: "Blog found" });
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal server error");
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
