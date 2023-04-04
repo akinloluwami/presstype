b039ed2edea715c9e1b5a5276c5cabba86475f46
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 12:51:41 +0100

    axios post req

--- actions/auth/complete.ts
+++ actions/auth/complete.ts
@@ -1,16 +1,24 @@
 import axios from "axios";
 
-const completeSignUp = async ({
-  title,
-  about,
-  subdomain,
-}: {
-  title: string;
-  about: string;
-  subdomain: string;
-}) => {
+const completeSignUp = async (
+  {
+    title,
+    about,
+    subdomain,
+  }: {
+    title: string;
+    about: string;
+    subdomain: string;
+  },
+  token: string
+) => {
   const data = { title, about, subdomain };
 
   try {
+    axios.post("/api/blogs/complete", data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   } catch (error) {}
 };
