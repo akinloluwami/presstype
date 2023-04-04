8cf6e00aad80bc3b0678d990071e6eca0fa65e15
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 16:48:09 +0100

    Blog schema

--- schema/BlogPost.ts
+++ schema/BlogPost.ts
@@ -1,6 +1,27 @@
 import { Schema, model, models } from "mongoose";
 
-const blogPostSchema = new Schema({});
+const blogPostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const BlogPost = models.BlogPost || model("BlogPost", blogPostSchema);
 
