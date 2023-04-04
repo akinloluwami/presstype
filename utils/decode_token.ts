5e023f225c3e682223eff9e900186bd85734ab39
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 13:21:09 +0100

    check for expiration

--- utils/decode_token.ts
+++ utils/decode_token.ts
@@ -1,8 +1,21 @@
 import DecodedToken from "@/interfaces/DecodedToken";
 import jwt from "jsonwebtoken";
+import dayjs from "dayjs";
 
-const decodeToken = (token: string): DecodedToken => {
-  return jwt.verify(token, process.env.JWT_SECRET!) as unknown as DecodedToken;
+const decodeToken = (token: string): DecodedToken | null => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as DecodedToken;
+    const now = dayjs();
+    const expirationTime = dayjs.unix(decoded.exp);
+
+    if (now.isAfter(expirationTime)) {
+      return null;
+    }
+
+    return decoded;
+  } catch (err) {
+    return null;
+  }
 };
 
 export default decodeToken;
