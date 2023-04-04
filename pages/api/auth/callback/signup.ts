3b114b6593d7f06e06a827b74c9c1e00072f4276
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 15:43:36 +0100

    removed unused imports

--- pages/api/auth/callback/signup.ts
+++ pages/api/auth/callback/signup.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "@/utils/db";
 import AuthToken from "@/schema/AuthToken";
-import dayjs from "dayjs";
-import jwt from "jsonwebtoken";
 import Blog from "@/schema/Blog";
 import decodeToken from "@/utils/decode_token";
 
