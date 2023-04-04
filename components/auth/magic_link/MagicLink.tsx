1478501a126d4fffd7e127d68c85e478444cc612
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 11:37:29 +0100

    ___

--- components/auth/magic_link/MagicLink.tsx
+++ components/auth/magic_link/MagicLink.tsx
@@ -19,12 +19,10 @@ const MagicLink = ({ type }: MagicLinkProps) => {
     if (type === "Sign in") {
       const blog: any = await blogExists(email);
       if (blog.status === 200) {
-        console.log(blog);
         setMessage(blog.data.message);
         setLoading(false);
         return;
       }
-      console.log(blog);
       setMessage(blog.data.message);
       setLoading(false);
     }
