87c93f36a0f014e9c776804cb2ebac02386bb4f2
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 11:32:45 +0100

    fixed endpoint

--- actions/auth/exists.ts
+++ actions/auth/exists.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const blogExists = async (email: string) => {
   try {
-    const response = await axios.get(`/api/auth/exists?email=${email}`);
+    const response = await axios.get(`/api/blogs/exists?email=${email}`);
     return response;
   } catch (error) {
     return error;
