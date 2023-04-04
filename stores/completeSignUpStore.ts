c675856c666e4911ff27aa079a6cf931ab1c31c6
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 13:06:20 +0100

    complete signup store

---
+++ stores/completeSignUpStore.ts
@@ -0,0 +1,23 @@
+import create from "zustand";
+
+type CompleteSignupStore = {
+  step: number;
+  title: string;
+  subdomain: string;
+  about: string;
+  setStep: (step: number) => void;
+  setTitle: (title: string) => void;
+  setSubdomain: (subdomain: string) => void;
+  setAbout: (about: string) => void;
+};
+
+export const useCompleteSignupStore = create<CompleteSignupStore>((set) => ({
+  step: 0,
+  title: "",
+  subdomain: "",
+  about: "",
+  setStep: (step: number) => set(() => ({ step })),
+  setTitle: (title: string) => set(() => ({ title })),
+  setSubdomain: (subdomain: string) => set(() => ({ subdomain })),
+  setAbout: (about: string) => set(() => ({ about })),
+}));
