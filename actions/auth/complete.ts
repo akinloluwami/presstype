caf9181ee7e2c6f13147cea9732bdc1e86ca7bd7
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 12:49:17 +0100

    data

---
+++ actions/auth/complete.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const completeSignUp = async ({
+  title,
+  about,
+  subdomain,
+}: {
+  title: string;
+  about: string;
+  subdomain: string;
+}) => {
+  const data = { title, about, subdomain };
+
+  try {
+  } catch (error) {}
+};
