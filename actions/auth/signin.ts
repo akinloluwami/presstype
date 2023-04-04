5cf40d23592348f542d9bc20e9b148ebe10544bb
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 11:46:16 +0100

    returned response

--- actions/auth/signin.ts
+++ actions/auth/signin.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const singIn = async (email: string) => {
   try {
+    const response = await axios.post(`/api/auth/signinwithmagiclink`, {
+      email,
+    });
+    return response;
   } catch (error: any) {
     return error.response;
   }
