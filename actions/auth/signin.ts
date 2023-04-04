da31ee714186b6f0533159db7e238db31277a648
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-04 11:43:50 +0100

    added try catch block

--- actions/auth/signin.ts
+++ actions/auth/signin.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-const singIn = async (email: string) => {};
+const singIn = async (email: string) => {
+  try {
+  } catch (error: any) {
+    return error.response;
+  }
+};
 
 export default singIn;
