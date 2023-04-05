1f6e6962ecfe5d156f5694076932931e71dfaa0f
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-05 10:04:15 +0100

    editor

--- components/dashboard/editor/Editor.tsx
+++ components/dashboard/editor/Editor.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 const Editor = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World! 🌎️</p>",
+  });
   return (
     <div contentEditable={true}>
       <code>Start with something...</code>
