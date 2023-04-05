6fdddd77637f14eb62842436bb6a146f05a09675
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-05 15:35:09 +0100

    Link configure

--- components/dashboard/editor/Editor.tsx
+++ components/dashboard/editor/Editor.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { useEditor, EditorContent, FloatingMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import EditorMenu from "./Menu";
+import Link from "@tiptap/extension-link";
 
 const Editor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Link.configure({
+        openOnClick: false,
+      }),
+    ],
     content: "<p>Hello World! 🌎️</p>",
   });
   return (
