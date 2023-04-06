22dd0242ea16aec94ebda7d230e89686c417c6fa
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-06 12:35:33 +0100

    Bubble Menu

--- components/dashboard/editor/Editor.tsx
+++ components/dashboard/editor/Editor.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { useEditor, EditorContent, FloatingMenu } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  FloatingMenu,
+  BubbleMenu,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import EditorMenu from "./Menu";
 import Link from "@tiptap/extension-link";
@@ -23,6 +28,28 @@ const Editor = () => {
   });
   return (
     <>
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive("bold") ? "is-active" : ""}
+          >
+            bold
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive("italic") ? "is-active" : ""}
+          >
+            italic
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive("strike") ? "is-active" : ""}
+          >
+            strike
+          </button>
+        </BubbleMenu>
+      )}
       <EditorContent editor={editor} className={styles.editor_content} />
       <EditorMenu editor={editor} />
     </>
