7019d1bea279568b6c193d7b4633e79656f5a25d
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-06 18:30:35 +0100

    created unset link function

--- components/dashboard/editor/LinkSelectMenu.tsx
+++ components/dashboard/editor/LinkSelectMenu.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LinkSelectMenu = ({ editor }: any) => {
+  const [linkHref, setLinkHref] = useState<string | undefined>(
+    editor.isActive("link") ? editor.getAttributes("link").href : undefined
+  );
+
+  const handleChangeLinkHref = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLinkHref(event.target.value);
+  };
+
+  const handleApplyLinkHref = () => {
+    if (linkHref !== undefined) {
+      editor
+        .chain()
+        .focus()
+        .extendMarkRange("link")
+        .setLink({ href: linkHref })
+        .run();
+    }
+  };
+
+  const handleUnsetLink = () => {
+    editor.chain().focus().unsetLink().run();
+  };
+
   return (
     <div>
-      <button onClick={() => editor.chain().focus().unsetLink().run()}>
-        Unset link
-      </button>
+      <button onClick={handleUnsetLink}>Unset link</button>
+      <div>
+        <label htmlFor="link-href-input">Link address:</label>
+        <input
+          id="link-href-input"
+          type="text"
+          value={linkHref || ""}
+          onChange={handleChangeLinkHref}
+        />
+        <button onClick={handleApplyLinkHref}>Apply</button>
+      </div>
     </div>
   );
 };
