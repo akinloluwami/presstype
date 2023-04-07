e50964d7be9ba1ec383b14f2297eb23128bbb07b
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-07 13:06:19 +0100

    Modal

--- components/dashboard/editor/Menu.tsx
+++ components/dashboard/editor/Menu.tsx
@@ -12,6 +12,7 @@ import {
 import { BiCodeBlock } from "react-icons/bi";
 import { BsCode, BsImage } from "react-icons/bs";
 import { TbDivide } from "react-icons/tb";
+import Modal from "@/components/elements/modal/Modal";
 
 const EditorMenu = ({ editor }: any) => {
   if (!editor) {
@@ -25,95 +26,115 @@ const EditorMenu = ({ editor }: any) => {
     }
   }, [editor]);
 
+  const onClose = () => {};
   return (
-    <div className={styles.editor_menu}>
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "is-active" : ""}
-      >
-        <MdFormatBold /> Bold
-      </button>
+    <>
+      <Modal onClose={onClose}>
+        <h1>Babe</h1>
+      </Modal>
+      <div className={styles.editor_menu}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is-active" : ""}
+        >
+          <MdFormatBold /> Bold
+        </button>
 
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={editor.isActive("italic") ? "is-active" : ""}
-      >
-        <MdFormatItalic /> Italic
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
-      >
-        <MdStrikethroughS /> Strike
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={!editor.can().chain().focus().toggleCode().run()}
-        className={editor.isActive("code") ? "is-active" : ""}
-      >
-        <BsCode /> Code
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive("paragraph") ? "is-active" : ""}
-      >
-        <MdOutlineTextFields /> Text
-      </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is-active" : ""}
+        >
+          <MdFormatItalic /> Italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is-active" : ""}
+        >
+          <MdStrikethroughS /> Strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          disabled={!editor.can().chain().focus().toggleCode().run()}
+          className={editor.isActive("code") ? "is-active" : ""}
+        >
+          <BsCode /> Code
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive("paragraph") ? "is-active" : ""}
+        >
+          <MdOutlineTextFields /> Text
+        </button>
 
-      <button onClick={addImage}>
-        <BsImage /> Image
-      </button>
+        <button onClick={addImage}>
+          <BsImage /> Image
+        </button>
 
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-      >
-        Heading 1
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-      >
-        Heading 2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-      >
-        Heading 3
-      </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+          }
+        >
+          Heading 1
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+          }
+        >
+          Heading 2
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+          }
+        >
+          Heading 3
+        </button>
 
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive("bulletList") ? "is-active" : ""}
-      >
-        <MdFormatListBulleted /> Bullet list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive("orderedList") ? "is-active" : ""}
-      >
-        <MdFormatListNumbered /> Numbered list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive("codeBlock") ? "is-active" : ""}
-      >
-        <BiCodeBlock /> Code block
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive("blockquote") ? "is-active" : ""}
-      >
-        <MdFormatQuote /> Quote
-      </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        <TbDivide /> Divider
-      </button>
-    </div>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+        >
+          <MdFormatListBulleted /> Bullet list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is-active" : ""}
+        >
+          <MdFormatListNumbered /> Numbered list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={editor.isActive("codeBlock") ? "is-active" : ""}
+        >
+          <BiCodeBlock /> Code block
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive("blockquote") ? "is-active" : ""}
+        >
+          <MdFormatQuote /> Quote
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setHorizontalRule().run()}
+        >
+          <TbDivide /> Divider
+        </button>
+      </div>
+    </>
   );
 };
 
--- components/elements/modal/Modal.tsx
+++ components/elements/modal/Modal.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./style.module.scss";
+import { FaTimes } from "react-icons/fa";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({
+  children,
+  onClose,
+}: {
+  children: React.ReactNode;
+  onClose: () => void;
+}) => {
+  return (
+    <div className={styles["modal-overlay"]}>
+      <div className={styles["modal-container"]}>
+        <button className={styles["modal-close-button"]} onClick={onClose}>
+          <FaTimes />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
