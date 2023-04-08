887853c7ebeb5f0f68a49a79c230d10114274cee
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-08 21:30:10 +0100

    added publish button

--- components/dashboard/posts/NewPostHeader/NewPostHeader.tsx
+++ components/dashboard/posts/NewPostHeader/NewPostHeader.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const NewPostHeader = () => {
-  return <div className={styles.new_post_header}></div>;
+  return (
+    <div className={styles.new_post_header}>
+      <button>Publish</button>
+    </div>
+  );
 };
 
 export default NewPostHeader;
