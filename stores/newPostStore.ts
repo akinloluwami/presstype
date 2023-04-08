8393ca4a0b9f8f1cf247b868392919c1ceb0bd82
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-08 21:37:10 +0100

    created new post store

---
+++ stores/newPostStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+interface NewPostStore {
+  title: string;
+  content: string;
+  cover_image?: string;
+  setTitle: (title: string) => void;
+  setContent: (content: string) => void;
+  setCoverImage?: (cover_image: string) => void;
+}
+
+export const useNewPostStore = create<NewPostStore>((set) => ({
+  title: "",
+  content: "",
+  cover_image: "",
+  setTitle: (title: string) => set(() => ({ title })),
+  setContent: (content: string) => set(() => ({ content })),
+  setCoverImage: (cover_image: string) => set(() => ({ cover_image })),
+}));
