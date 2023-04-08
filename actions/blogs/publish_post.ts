d7503025bb8ef294c2ade56dbd7c618c03e44996
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-08 21:46:11 +0100

    got content and title from useNewPostStore

--- actions/blogs/publish_post.ts
+++ actions/blogs/publish_post.ts
@@ -1,3 +1,9 @@
-const publishPost = async () => {};
+import { useNewPostStore } from "@/stores/newPostStore";
+
+const publishPost = async () => {
+  const { title, content } = useNewPostStore();
+
+  const data = { content, title };
+};
 
 export default publishPost;
