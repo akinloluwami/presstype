36b34952c20c48679b5e8b35396b83287309e2f0
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-08 21:12:51 +0100

    imported NewPostHeader component

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -2,10 +2,12 @@ import Editor from "@/components/dashboard/editor/Editor";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 import styles from "./styles.module.scss";
+import NewPostHeader from "@/components/dashboard/posts/NewPostHeader/NewPostHeader";
 
 const NewPost = () => {
   return (
     <DashboardLayout page_name={"Add new post"}>
+      <NewPostHeader />
       <input
         type="text"
         className={styles.article_title}
