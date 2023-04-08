ef2215389621d5d81c1bfcdc4da9033f28246d4a
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-08 21:40:11 +0100

    setTitle

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -3,15 +3,18 @@ import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
 import styles from "./styles.module.scss";
 import NewPostHeader from "@/components/dashboard/posts/NewPostHeader/NewPostHeader";
+import { useNewPostStore } from "@/stores/newPostStore";
 
 const NewPost = () => {
+  const { title, setTitle } = useNewPostStore();
   return (
-    <DashboardLayout page_name={"Add new post"}>
+    <DashboardLayout>
       <NewPostHeader />
       <input
         type="text"
         className={styles.article_title}
         placeholder="Article title"
+        onChange={(e) => setTitle(e.target.value)}
       />
       <div className={styles.editor}>
         <Editor />
