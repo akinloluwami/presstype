382a548e5613a8886f309c201bb1c92c4f1a5f2e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-08 20:56:17 +0100

    added input field

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -1,14 +1,19 @@
 import Editor from "@/components/dashboard/editor/Editor";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import React from "react";
+import styles from "./styles.module.scss";
+
 const NewPost = () => {
   return (
-    <DashboardLayout
-      page_name={"Add new post"}
-      showButton={true}
-      button={<button>Publish</button>}
-    >
-      <Editor />
+    <DashboardLayout page_name={"Add new post"}>
+      <input
+        type="text"
+        className={styles.article_title}
+        placeholder="Article title"
+      />
+      <div className={styles.editor}>
+        <Editor />
+      </div>
     </DashboardLayout>
   );
 };
