eb010a88ad0a4bbd72ece2cdfa4c15285cc2668f
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-09 18:45:13 +0100

    changed .body.file

--- pages/api/blogs/upload-image.ts
+++ pages/api/blogs/upload-image.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "@/middlewares/allowMethods";
+
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -11,8 +12,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     region: process.env.AWS_REGION as string,
   });
 
-  const file = req.body.file;
-  const fileName = req.body.fileName;
+  const body = JSON.parse(req.body);
+
+  const file = Buffer.from(
+    body.file.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  );
+  const fileName = body.fileName;
 
   if (!file) {
     res.status(400).send("No file");
@@ -24,26 +30,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const base64Data = file.replace(/^data:image\/\w+;base64,/, "");
-  const buffer = Buffer.from(base64Data, "base64");
-
-  const params: {
-    Bucket: string;
-    Key: string;
-    Body: any;
-  } = {
+  const params = {
     Bucket: process.env.AWS_BUCKET_NAME as string,
     Key: fileName,
-    Body: buffer,
+    Body: file,
   };
 
   try {
-    const result = await s3.send(new PutObjectCommand(params));
-    console.log(result);
+    const response = await s3.send(new PutObjectCommand(params));
+    console.log("File uploaded successfully. ", response);
     res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
+  } catch (err: any) {
+    console.log("Error uploading file: ", err);
+    res.status(500).send(err.message);
   }
 };
 
