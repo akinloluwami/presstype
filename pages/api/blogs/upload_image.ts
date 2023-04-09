68c59689969ac76a621ead8942732749dc927174
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-09 10:56:12 +0100

    installed aws-sdk

--- pages/api/blogs/upload_image.ts
+++ pages/api/blogs/upload_image.ts
@@ -1,24 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "@/middlewares/allowMethods";
-
-//create a function to upload images to s3
-
+import AWS from "aws-sdk";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  //s3 config
-
-  const AWS = require("aws-sdk");
   const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
   });
 
-  //get the image from the request
-
   const file = req.body.file;
   const fileName = req.body.fileName;
-
-  //upload the image to s3
 
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
