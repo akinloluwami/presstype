4d60b057202ca6e2ed8f3a6c1b925bd63287f53a
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 12:15:08 +0100

    crrate refresh token

---
+++ utils/create_refresh_token.ts
@@ -0,0 +1,28 @@
+// createRefreshToken.ts
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+
+export const createRefreshToken = (userId: string) => {
+  const refreshToken = uuidv4();
+  const refreshTokenExpiresIn = "7d"; // for example, refresh tokens expire in 7 days
+
+  const refreshTokenPayload = {
+    sub: userId,
+    jti: refreshToken,
+  };
+
+  const refreshTokenOptions: jwt.SignOptions = {
+    expiresIn: refreshTokenExpiresIn,
+  };
+
+  const signedRefreshToken = jwt.sign(
+    refreshTokenPayload,
+    process.env.JWT_REFRESH_SECRET!,
+    refreshTokenOptions
+  );
+
+  return {
+    refreshToken: signedRefreshToken,
+    refreshTokenExpiresIn: refreshTokenExpiresIn,
+  };
+};
