44d3bfee5e412e2e90a7523c0e20b955dd5d0fa4
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 17:28:58 +0100

    ___

--- pages/api/blogs/posts/create.ts
+++ pages/api/blogs/posts/create.ts
@@ -2,9 +2,8 @@ import Blog from "@/schema/Blog";
 import { NextApiRequest, NextApiResponse } from "next";
 import DecodedToken from "@/interfaces/DecodedToken";
 import decodeToken from "@/utils/decode_token";
-// import { allowMethods } from "@/middlewares/allowMethods";
+import { allowMethods } from "@/middlewares/allowMethods";
 import BlogPost from "@/schema/BlogPost";
-import { allowMethods } from "next-method-guard";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!req.headers.authorization) {
