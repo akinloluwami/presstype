27903ccf1d0ef3152c5298c21fdd77796961f3a0
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 01:56:29 +0100

    ugh

--- pages/api/blogs/upload-image.ts
+++ pages/api/blogs/upload-image.ts
@@ -1,9 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "@/middlewares/allowMethods";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import multer from "multer";
-
-const upload = multer({ dest: "uploads/" });
+import { ListBucketsCommand } from "@aws-sdk";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const s3 = new S3Client({
@@ -14,46 +12,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     region: process.env.AWS_REGION as string,
   });
 
-  const file = req.file;
-  const fileName = req.body.fileName;
-
-  if (!file) {
-    res.status(400).send("No file");
-    return;
-  }
-
-  if (!fileName) {
-    res.status(400).send("No fileName");
-    return;
-  }
-
-  const buffer = file.buffer;
-
-  const params: {
-    Bucket: string;
-    Key: string;
-    Body: any;
-  } = {
-    Bucket: process.env.AWS_BUCKET_NAME as string,
-    Key: fileName,
-    Body: buffer,
-  };
-
-  try {
-    const result = await s3.send(new PutObjectCommand(params));
-    console.log(result);
-    res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
+  const command = new ListBucketsCommand({});
+  const response = s3.send(command);
+  res.send((await response).Buckets);
 };
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-// export default allowMethods(["POST"])(upload.single("file"), handler);
-export default allowMethods(["POST"])([upload.single("file"), handler]);
+export default allowMethods(["POST"])(handler);
