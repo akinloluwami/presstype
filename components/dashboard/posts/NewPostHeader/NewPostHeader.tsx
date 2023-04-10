994765785913f1d480f6e2594b1cbef794197e15
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 14:06:49 +0100

    loading state

--- components/dashboard/posts/NewPostHeader/NewPostHeader.tsx
+++ components/dashboard/posts/NewPostHeader/NewPostHeader.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import usePublishPost from "@/actions/blogs/publish_post";
+import { toast, Toaster } from "react-hot-toast";
 
 const NewPostHeader = () => {
   const publishPost = usePublishPost();
-
+  const [loading, setLoading] = useState(false);
   const handle = async () => {
+    setLoading(true);
     const res = await publishPost();
-    console.log(res);
+    toast("Hi");
   };
   return (
     <div className={styles.new_post_header}>
+      <Toaster />
       <button onClick={handle}>Publish</button>
     </div>
   );
