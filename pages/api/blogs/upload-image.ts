3307e5f609479907a775d844680722a86e79a010
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 02:55:18 +0100

    __

--- pages/api/blogs/upload-image.ts
+++ pages/api/blogs/upload-image.ts
@@ -1,24 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "@/middlewares/allowMethods";
-import { S3 } from "@aws-sdk/client-s3-node";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const s3 = new S3({
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    },
-    region: process.env.AWS_REGION as string,
-  });
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 
   try {
-    const data = await s3.listBuckets({});
-    const buckets = data?.Buckets.map((bucket) => bucket.Name);
-    res.status(200).json(buckets);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to list buckets" });
+    const s3 = new S3Client({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+      },
+      region: process.env.AWS_REGION as string,
+    });
+
+    const key = `uploads/${uuidv4()}`;
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME as string,
+      Key: key,
+      Body: req.body,
+      ContentType: req.headers["content-type"],
+    };
+
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+
+    return res
+      .status(200)
+      .json({ url: `${process.env.AWS_CLOUDFRONT_DOMAIN}/${key}` });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Failed to upload file" });
   }
 };
 
-export default allowMethods(["GET"])(handler);
+export default allowMethods(["POST"])(handler);
