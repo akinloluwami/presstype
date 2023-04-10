441b66fed8da4cd3455b5c82efbe8d35c73ac1be
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 13:57:37 +0100

    handle

--- components/dashboard/posts/NewPostHeader/NewPostHeader.tsx
+++ components/dashboard/posts/NewPostHeader/NewPostHeader.tsx
@@ -4,9 +4,14 @@ import usePublishPost from "@/actions/blogs/publish_post";
 
 const NewPostHeader = () => {
   const publishPost = usePublishPost();
+
+  const handle = async () => {
+    const res = await publishPost();
+    console.log(res);
+  };
   return (
     <div className={styles.new_post_header}>
-      <button onClick={publishPost}>Publish</button>
+      <button onClick={handle}>Publish</button>
     </div>
   );
 };
