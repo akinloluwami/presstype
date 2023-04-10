79dc7711e7751d38e08f21f70b898988f61799aa
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 03:33:41 +0100

    back to square one

--- pages/api/blogs/upload-image.ts
+++ pages/api/blogs/upload-image.ts
@@ -1,39 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "@/middlewares/allowMethods";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { v4 as uuidv4 } from "uuid";
-import path from "path";
+import { S3Client } from "@aws-sdk/client-s3";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const s3 = new S3Client({
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-      },
-      region: process.env.AWS_REGION as string,
-    });
-
-    const ext = path.extname(req.headers["content-disposition"] || "");
-    const key = `uploads/${uuidv4()}${ext}`;
-
-    const params = {
-      Bucket: process.env.AWS_BUCKET_NAME as string,
-      Key: key,
-      Body: req.body,
-      ContentType: req.headers["content-type"],
-    };
-
-    const command = new PutObjectCommand(params);
-    await s3.send(command);
-
-    return res
-      .status(200)
-      .json({ url: `${process.env.AWS_CLOUDFRONT_DOMAIN}/${key}` });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Failed to upload file" });
-  }
+  const s3 = new S3Client({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
+    region: process.env.AWS_REGION as string,
+  });
 };
 
 export default allowMethods(["POST"])(handler);
