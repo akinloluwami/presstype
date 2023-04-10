0627cd7920b6b8c84caf75a5b4df7e0edbcfe1a7
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 14:02:59 +0100

    returned error

--- actions/blogs/publish_post.ts
+++ actions/blogs/publish_post.ts
@@ -5,14 +5,19 @@ import axios from "axios";
 const usePublishPost = () => {
   const { title, content } = useNewPostStore();
   const { token } = useTokenStore();
+
   const publishPost = async () => {
-    const payload = { content, title };
-    const response = await axios.post("/api/blogs/posts/create", payload, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response;
+    try {
+      const payload = { content, title };
+      const response = await axios.post("/api/blogs/posts/create", payload, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response;
+    } catch (error: any) {
+      return error.response;
+    }
   };
 
   return publishPost;
