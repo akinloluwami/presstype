70ba2a527e0e3d8ba539d53dc319c2eec6721849
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 11:22:02 +0100

    test

--- pages/posts/new/index.tsx
+++ pages/posts/new/index.tsx
@@ -1,14 +1,42 @@
 import Editor from "@/components/dashboard/editor/Editor";
 import DashboardLayout from "@/layouts/dashboard_layout";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import NewPostHeader from "@/components/dashboard/posts/NewPostHeader/NewPostHeader";
 import { useNewPostStore } from "@/stores/newPostStore";
+import axios from "axios";
 
 const NewPost = () => {
   const { title, setTitle } = useNewPostStore();
+  const [file, setFile] = useState<any>();
+
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post("/api/blogs/upload-image", formData).then((data) => {
+      console.log(data);
+    });
+  };
+
   return (
     <DashboardLayout page_name={`Editing ${title ? `"${title}"` : ""}`}>
+      <input
+        type="file"
+        onChange={(e: any) => {
+          setFile(e.target.files[0]);
+        }}
+      />
+
+      <button
+        onClick={() => {
+          console.log(file);
+
+          uploadImage();
+        }}
+      >
+        Upload
+      </button>
       <NewPostHeader />
       <input
         type="text"
