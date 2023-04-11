71433327f978d920a2991fb9f883450bf649d689
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-11 10:27:51 +0100

    added slug

--- pages/api/blogs/posts/create.ts
+++ pages/api/blogs/posts/create.ts
@@ -57,7 +57,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       content,
       cover_image,
       blog_id: blog._id,
-      // slug: title.toLowerCase().replace(/ /g, "-"),
+      slug: title.toLowerCase().replace(/ /g, "-"),
     });
 
     res.status(201).json({ message: "Post created" });
