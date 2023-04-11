181fe880810228b8f6c9f288bd1cdf4cd2d4885b
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-11 11:28:37 +0100

    validations

--- pages/api/blogs/posts/getAll.ts
+++ pages/api/blogs/posts/getAll.ts
@@ -6,6 +6,29 @@ import { allowMethods } from "@/middlewares/allowMethods";
 import BlogPost from "@/schema/BlogPost";
 import { connectToDatabase } from "@/utils/db";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  connectToDatabase();
+  if (!req.headers.authorization) {
+    res.status(401).json({
+      message: "Unauthorized request",
+    });
+    return;
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded: DecodedToken | null = decodeToken(token as string);
+
+  if (!decoded) {
+    res.status(400).json({ message: "Token has expired" });
+    return;
+  }
+
+  const blog = await Blog.findOne({ email: decoded.email });
+
+  if (!blog) {
+    res.status(404).json({ message: "Blog not found" });
+    return;
+  }
+};
 
 export default allowMethods(["GET"])(handler);
