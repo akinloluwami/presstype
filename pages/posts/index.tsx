9fd95b267652afb0f261b4fba7bec9a7e39f799e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-11 12:49:33 +0100

    moved to index

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,10 +2,26 @@ import NoPost from "@/components/dashboard/no_post/NoPost";
 import Posts from "@/components/dashboard/posts/Posts";
 import DashboardLayout from "@/layouts/dashboard_layout";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import useGetAllPosts from "@/actions/blogs/get_all_posts";
 
 const Index = () => {
+  const [posts, setPosts] = useState<[]>([]);
+
+  const getAllPosts = useGetAllPosts();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getAllPosts();
+
+      if (res.data.blogPosts) {
+        setPosts(res.data.blogPosts);
+        return;
+      }
+    })();
+  }, []);
+
   const router = useRouter();
   return (
     <DashboardLayout
@@ -15,8 +31,7 @@ const Index = () => {
         <button onClick={() => router.push("/posts/new")}>Add new</button>
       }
     >
-      {/* <NoPost /> */}
-      <Posts />
+      {posts.length < 1 ? <NoPost /> : <Posts posts={posts} />}
     </DashboardLayout>
   );
 };
