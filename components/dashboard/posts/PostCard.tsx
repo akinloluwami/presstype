8be8cbe867387268a6b4c80eb7e3c3c394070456
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-11 11:51:09 +0100

    added props

--- components/dashboard/posts/PostCard.tsx
+++ components/dashboard/posts/PostCard.tsx
@@ -3,11 +3,11 @@ import styles from "./styles.module.scss";
 import { CgOptions } from "react-icons/cg";
 import { useTheme } from "@/contexts/ThemeContext";
 
-const PostCard = () => {
+const PostCard = ({ title }: { title: string }) => {
   const { theme } = useTheme();
   return (
     <div className={styles.post_card_container}>
-      <h1>How to become a developer without learning anything</h1>
+      <h1>{title}</h1>
       <div className={styles.side}>
         <p className="">539 views</p>
         <button data-theme={theme}>
