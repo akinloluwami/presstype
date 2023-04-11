98b053a63f4058bf7920fec29914aae15b409935
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-11 22:29:06 +0100

    what is

--- publish/components/auth/signup_steps/PubNameAndDesc.tsx
+++ publish/components/auth/signup_steps/PubNameAndDesc.tsx
@@ -40,7 +40,7 @@ const PubNameAndDesc = () => {
         </div> */}
       </div>
       <div className={styles.pub_about}>
-        <p>What's your publication about?</p>
+        <p>What is your publication about?</p>
         <textarea
           placeholder="Describe your publication in 200 characters or less."
           value={about}
