c2cf01e386b6e8355dc8652925b5fa3d3dce2871
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-11 11:48:05 +0100

    res

--- components/dashboard/posts/Posts.tsx
+++ components/dashboard/posts/Posts.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostCard from "./PostCard";
 import styles from "./styles.module.scss";
+import useGetAllPosts from "@/actions/blogs/get_all_posts";
 
 const Posts = () => {
   // const postTypes = ["All", "Scheduled", "Deleted"];
+  const [posts, setPosts] = useState<[]>([]);
+
+  const getAllPosts = useGetAllPosts();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getAllPosts();
+    })();
+  }, []);
+
+  // useEffect(() => {}, []);
   return (
     <div className={styles.posts_container}>
       {/* <div className={styles.posts_types}>
