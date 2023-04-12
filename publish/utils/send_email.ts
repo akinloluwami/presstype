2307a98c612564adcf0e4b7f344fc969d1848499
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-12 03:01:12 +0100

    erm...

--- publish/utils/send_email.ts
+++ publish/utils/send_email.ts
@@ -1,19 +1,23 @@
 import transporter from "@/config/transporter";
 
 const sendMail = (to: string, subject: string, html: string) => {
-  const mailOptions = {
-    from: `"Akinkunmi from PressType" <${process.env.SMTP_USER}>`,
-    to,
-    subject,
-    html,
-  };
+  return new Promise((resolve, reject) => {
+    const mailOptions = {
+      from: `"Akinkunmi from PressType" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      html,
+    };
 
-  transporter.sendMail(mailOptions, (error, info: any) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(`Email sent: ${info.response}`);
-    }
+    transporter.sendMail(mailOptions, (error, info: any) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+        resolve(info.response);
+      }
+    });
   });
 };
 
