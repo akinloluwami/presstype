8303b9f74875399d34ab4ba95879dbeefedd7d29
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-16 00:58:22 +0100

    what did I change again?

--- publish/config/transporter.ts
+++ publish/config/transporter.ts
@@ -5,7 +5,7 @@ const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS, SMTP_SECURE } = process.env;
 const transporter: Transporter = nodemailer.createTransport({
   host: SMTP_HOST,
   port: Number(SMTP_PORT),
-  secure: true,
+  secure: SMTP_SECURE === "true",
   auth: {
     user: SMTP_USER,
     pass: SMTP_PASS,
