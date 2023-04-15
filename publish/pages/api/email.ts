cd56fb8fb605af199427fb0bba2a3f0822efbd56
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-15 18:43:43 +0100

    ___

--- publish/pages/api/email.ts
+++ publish/pages/api/email.ts
@@ -8,7 +8,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     "<p>This is a <b>test email</b> sent from <i>Nodemailer</i>!</p>";
 
   try {
-    sendMail(to, subject, html);
+    await sendMail(to, subject, html);
     res.status(200).send("Test email sent!");
   } catch (error) {
     console.error(error);
