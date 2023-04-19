4eab2f3152ceed1b63b35958433be4ebd3feb30e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 22:17:19 +0100

    fixed env name

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectToDB = mongoose.connect(process.env.MONGO_URI || "", {
+const connectToDB = mongoose.connect(process.env.MONGODB_URI || "", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
