f326f97d3e56a4cffb79a0abe2eff5616520176e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 22:17:27 +0100

    connected to db

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import connectToDB from "./utils/db";
+
+connectToDB;
 
 dotenv.config();
 
