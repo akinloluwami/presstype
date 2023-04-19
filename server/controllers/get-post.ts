b726b84d0a701bf13a8353b6789421fa932dcb3f
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-20 00:46:16 +0100

    200

--- server/controllers/get-post.ts
+++ server/controllers/get-post.ts
@@ -6,7 +6,27 @@ const getBlogPost = async (req: Request, res: Response) => {
   const { blog, slug } = req.query;
 
   try {
-  } catch (error) {}
+    const blogPage = await Blog.findOne({ subdomain: blog });
+    if (!blogPage) return res.status(404).json({ error: "Blog not found" });
+    const post = await BlogPost.findOne({
+      blog_id: blogPage._id,
+      slug: blogPage.slug,
+    });
+
+    const { title, content, createdAt, cover_image, slug } = post;
+
+    const postInfo = {
+      title,
+      content,
+      createdAt,
+      cover_image,
+      slug,
+    };
+
+    return res.status(200).json({ post: postInfo });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export default getBlogPost;
