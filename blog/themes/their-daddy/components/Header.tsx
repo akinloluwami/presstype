5d1a47e5071f67ecb8906b622529976e707f83fd
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 23:57:00 +0100

    increased fontsize

--- blog/themes/their-daddy/components/Header.tsx
+++ blog/themes/their-daddy/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Header = ({ title }: { title: string }) => {
+const Header = ({ title, about }: { title: string; about?: string }) => {
   return (
     <div className="h-fit py-12">
-      <h1>{title}</h1>
+      <h1 className="text-5xl">{title}</h1>
+      <p className="text-lg">{about}</p>
     </div>
   );
 };
