a9e0950f8bc34e7ffa95b9d9872537eba46696db
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 20:56:55 +0100

    returned blog info and posts

--- blog/pages/api/[blog].ts
+++ blog/pages/api/[blog].ts
@@ -1,12 +1,33 @@
+import Blog from "@/schema/Blog";
+import BlogPost from "@/schema/BlogPost";
 import { connectToDatabase } from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
-import Blog from "@";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await connectToDatabase();
     const { blog } = req.query;
+
+    const blogExist = await Blog.findOne({ subdomain: blog });
+
+    if (!blogExist) {
+      res.status(404).json({
+        message: "Blog not found",
+      });
+      return;
+    }
+
+    const { title, about } = blogExist;
+
+    const blogInfo = { title, about };
+
+    const blogPosts = await BlogPost.find(
+      { blog_id: blogExist._id },
+      { title: 1, slug: 1 }
+    );
+
+    res.status(200).json({ blogInfo, blogPosts });
   } catch (error) {
     res.status(500).json({
       message: "Something went wrong.",
