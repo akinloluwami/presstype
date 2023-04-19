2b41c13aa6c2727df87daaa1672ae2fe795f1253
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 21:37:48 +0100

    axios

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -5,4 +5,10 @@ const Index = () => {
   return <div>Index</div>;
 };
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+ try{
+  const response  = await axios
+ }
+};
+
 export default Index;
