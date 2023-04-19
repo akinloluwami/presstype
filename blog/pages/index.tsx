7c4b724664f5146bacaddbfc1a905fadf016f25f
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 22:31:01 +0100

    test fetch

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -3,20 +3,29 @@ import { GetServerSideProps } from "next";
 import React, { useEffect } from "react";
 
 const Index = ({ blog }: any) => {
+  useEffect(() => {
+    const fetchBlog = async () => {
+      const response = await axios.get("http://localhost:3310/blog");
+      const blog = response.data;
+      console.log(blog);
+    };
+    fetchBlog();
+    console.log(blog);
+  }, []);
+
   if (!blog) {
     return <div>Not Blog</div>;
   }
 
-  useEffect(() => {
-    console.log(blog);
-  }, []);
   return <div className="">wow, blog</div>;
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
-    const response = await axios.get("http://localhost:3310/");
+    const response = await axios.get("http://localhost:3310/blog");
     const blog = response.data;
+    console.log(blog);
+
     return { props: { blog } };
   } catch (error) {
     console.log(error);
