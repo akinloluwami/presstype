e392a7a7f91a1080709188ab4f905ba6dd38231e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 22:18:55 +0100

    lol

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectToDB = mongoose.connect(process.env.MONGODB_URI || "", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectToDB = mongoose.connect(process.env.MONGODB_URI || "");
 
 connectToDB
   .then(() => {
