1b30b6bce3c1a4f5edbe6c7a00aa74dbdd579e76
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 21:14:13 +0100

    added props

--- blog/themes/their-daddy/TheirDaddy.tsx
+++ blog/themes/their-daddy/TheirDaddy.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import BlogInfo from "@/types/blog-info";
+import BlogPost from "@/types/blog-post";
 
-const TheirDaddy = () => {
+const TheirDaddy = ({
+  blogInfo,
+  blogPosts,
+}: {
+  blogInfo: BlogInfo;
+  blogPosts: BlogPost[];
+}) => {
   return <div>TheirDaddy</div>;
 };
 
