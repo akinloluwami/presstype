e99503ca3b06d445c3df0359884becefbdc87516
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 20:14:07 +0100

    try catch

--- blog/pages/api/[blog].ts
+++ blog/pages/api/[blog].ts
@@ -1,6 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong.",
+    });
+  }
+};
 
 export default allowMethods(["GET"])(handler);
