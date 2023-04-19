f80919886be325aad3f079ac71086abbcf82006c
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 20:13:02 +0100

    db

---
+++ blog/utils/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connectToDatabase() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error("MongoDB URI not provided.");
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+export async function disconnectFromDatabase() {
+  await mongoose.disconnect();
+  console.log("Disconnected from MongoDB");
+}
