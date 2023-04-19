99a0e3dfe0e467a502f49ef7df39a393120b3605
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 22:13:51 +0100

    test

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Sever is live on http://localhost:${process.env.PORT}`)
+);
