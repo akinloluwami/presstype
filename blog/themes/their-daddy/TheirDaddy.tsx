2da499947e8d5d09f191fb03352701423bb1421f
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 23:20:29 +0100

    okay

--- blog/themes/their-daddy/TheirDaddy.tsx
+++ blog/themes/their-daddy/TheirDaddy.tsx
@@ -12,11 +12,13 @@ const TheirDaddy = ({
   blogPosts: BlogPost[];
 }) => {
   return (
-    <div>
-      <Header title={blogInfo.title} />
-      {blogPosts.map((post) => (
-        <PostCard title={post.title} slug={post.slug} key={post.slug} />
-      ))}
+    <div className="bg-black min-h-screen text-white">
+      <div className="w-[95%] max-w-[1100px] mx-auto">
+        <Header title={blogInfo.title} />
+        {blogPosts.map((post) => (
+          <PostCard title={post.title} slug={post.slug} key={post.slug} />
+        ))}
+      </div>
     </div>
   );
 };
