69da08342d4053dfec649abba5b2ecdaea7d6277
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 21:49:15 +0100

    server initial commit

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,14 +1,27 @@
+import axios from "axios";
 import { GetServerSideProps } from "next";
-import React from "react";
+import React, { useEffect } from "react";
 
-const Index = () => {
-  return <div>Index</div>;
+const Index = ({ blog }: any) => {
+  if (!blog) {
+    return <div>Not Blog</div>;
+  }
+
+  useEffect(() => {
+    console.log(blog);
+  }, []);
+  return <div className="">wow, blog</div>;
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
- try{
-  const response  = await axios
- }
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const response = await axios.get("/api");
+    const blog = response.data;
+    return { props: { blog } };
+  } catch (error) {
+    console.log(error);
+    return { props: { blog: null } };
+  }
 };
 
 export default Index;
