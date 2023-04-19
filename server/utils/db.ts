cbcc0be764084550752453ea8c76f71714269570
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-19 22:13:32 +0100

    connect to db

---
+++ server/utils/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectToDB = mongoose.connect(process.env.MONGO_URI || "", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connectToDB
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+export default connectToDB;
