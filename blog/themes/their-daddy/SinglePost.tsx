f79a7829339bab9f5868e31d1d30a152b149b7dc
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-20 01:59:10 +0100

    parsed html

--- blog/themes/their-daddy/SinglePost.tsx
+++ blog/themes/their-daddy/SinglePost.tsx
@@ -1,13 +1,14 @@
 import BlogPost from "@/types/blog-post";
 import React from "react";
 import moment from "moment";
+import parse from "react-html-parser";
 
 const SinglePost = ({ title, content, createdAt }: BlogPost) => {
   return (
     <div>
       <h1>{title}</h1>
       <p>{moment(createdAt).format("DD/MM/YYYY")}</p>
-      <div className="">{content}</div>
+      <div className="">{parse(content)}</div>
     </div>
   );
 };
