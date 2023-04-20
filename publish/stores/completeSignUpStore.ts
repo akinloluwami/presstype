4d739a2ee0f1cc18108effaf2da62dcc5f450705
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-20 13:00:29 +0100

    added new fields

--- publish/stores/completeSignUpStore.ts
+++ publish/stores/completeSignUpStore.ts
@@ -5,11 +5,15 @@ type CompleteSignupStore = {
   title: string;
   subdomain: string;
   description: string;
+  author_name: string;
+  author_bio: string;
+  setAuthorName: (author_name: string) => void;
   message: string;
   setStep: (step: number) => void;
   setTitle: (title: string) => void;
   setSubdomain: (subdomain: string) => void;
   setDescription: (description: string) => void;
+  setAuthorBio: (author_bio: string) => void;
   setMessage: (message: string) => void;
 };
 
@@ -18,10 +22,14 @@ export const useCompleteSignupStore = create<CompleteSignupStore>((set) => ({
   title: "",
   subdomain: "",
   description: "",
+  author_name: "",
+  author_bio: "",
   message: "",
   setStep: (step: number) => set(() => ({ step })),
   setTitle: (title: string) => set(() => ({ title })),
   setSubdomain: (subdomain: string) => set(() => ({ subdomain })),
   setDescription: (description: string) => set(() => ({ description })),
+  setAuthorName: (author_name: string) => set(() => ({ author_name })),
+  setAuthorBio: (author_bio: string) => set(() => ({ author_bio })),
   setMessage: (message: string) => set(() => ({ message })),
 }));
