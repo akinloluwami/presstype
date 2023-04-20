726f285923c46c0f2b5701883828bbf570e3bf8f
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-20 01:54:24 +0100

    ___

--- blog/pages/posts/[slug].tsx
+++ blog/pages/posts/[slug].tsx
@@ -2,12 +2,23 @@ import { GetServerSideProps } from "next";
 import React from "react";
 import axios from "axios";
 import BlogPost from "@/types/blog-post";
+import SinglePost from "@/themes/their-daddy/SinglePost";
 
 const Post = ({ post }: any) => {
   if (!post) {
     return <>Post not found</>;
   }
-  return <div>Post</div>;
+  return (
+    <div>
+      <SinglePost
+        title={post.title}
+        content={post.content}
+        createdAt={post.createdAt}
+        slug={post.slug}
+      />
+      {JSON.stringify(post)}
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
@@ -15,7 +26,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const response = await axios.get(
       `http://localhost:3310/post?url=${req.headers.host}${req.url}`
     );
-    const post = response.data;
+    const post = response.data.post;
     return { props: { post } };
   } catch (error) {
     console.log(error);
