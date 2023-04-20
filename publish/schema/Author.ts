964e8dfd2cf11c2e60bda5b43d05e1c2d275d51c
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-20 03:08:23 +0100

    author model

---
+++ publish/schema/Author.ts
@@ -0,0 +1,35 @@
+import { Schema, model, models } from "mongoose";
+
+const authorSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Author = models.Author || model("Author", authorSchema);
