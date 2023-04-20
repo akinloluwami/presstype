ac800b77fdbcd20e4b9c8975bec99b4ac45e85f4
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-20 02:17:04 +0100

    ___

--- blog/components/CustomLink.tsx
+++ blog/components/CustomLink.tsx
@@ -4,16 +4,17 @@ import { useRouter } from "next/router";
 interface LinkProps {
   href: string;
   children: React.ReactNode;
+  className: string;
 }
 
-const CustomLink = ({ href, children }: LinkProps) => {
+const CustomLink = ({ href, children, className }: LinkProps) => {
   const router = useRouter();
   const handleClick = (e: any) => {
     e.preventDefault();
     router.push(href, undefined, { shallow: false, scroll: true, force: true });
   };
   return (
-    <a href={href} onClick={handleClick}>
+    <a href={href} onClick={handleClick} className={className}>
       {children}
     </a>
   );
