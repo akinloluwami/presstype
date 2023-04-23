ecaca16e813e4e6f9dafe8527e1c22a7bf228e6a
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 09:59:08 +0100

    set state

--- publish/components/dashboard/BlogsList.tsx
+++ publish/components/dashboard/BlogsList.tsx
@@ -1,18 +1,19 @@
 import getAllBlogs from "@/actions/blogs/get_all_blogs";
 import { useTokenStore } from "@/stores/tokenStore";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const BlogsList = () => {
   const { token } = useTokenStore();
-
+  const [blogs, setBlogs] = useState([]);
   useEffect(() => {
     (async () => {
       const res = await getAllBlogs(token);
       console.log(res);
+      setBlogs(res.blogs);
     })();
   }, []);
 
-  return <div>BlogsList</div>;
+  return <select></select>;
 };
 
 export default BlogsList;
