34318888ddd2a051947b5e8db15bf6c8108be550
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 11:36:30 +0100

    refactor

--- publish/actions/blogs/get_all_posts.ts
+++ publish/actions/blogs/get_all_posts.ts
@@ -1,28 +1,19 @@
-import { useBlogStore } from "@/stores/blogStore";
-import { useTokenStore } from "@/stores/tokenStore";
 import axios from "axios";
 
-const useGetAllPosts = () => {
-  const { token } = useTokenStore();
-  const { blogId } = useBlogStore();
-
-  const getAllPosts = async () => {
-    try {
-      const response = await axios.get(
-        `/api/blogs/posts/get-all?blogId=${blogId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      return response;
-    } catch (error: any) {
-      return error.response;
-    }
-  };
-
-  return getAllPosts;
+const getAllPosts = async (token: string, blogId: string) => {
+  try {
+    const response = await axios.get(
+      `/api/blogs/posts/get-all?blogId=${blogId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response;
+  } catch (error: any) {
+    return error.response;
+  }
 };
 
-export default useGetAllPosts;
+export default getAllPosts;
