b47e2c0ddf8aed86088e07678d348ff3abc9a3fa
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 09:49:59 +0100

    added token as a parameter

--- publish/actions/blogs/get_all_blogs.ts
+++ publish/actions/blogs/get_all_blogs.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const getAllBlogs = async () => {
+const getAllBlogs = async (token: string) => {
   try {
-    const res = await axios.get("/api/blogs/get-author-blogs");
+    const res = await axios.get("/api/blogs/get-author-blogs", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return res.data;
   } catch (e: any) {
     return e.response;
