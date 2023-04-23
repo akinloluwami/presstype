d58ec7e15da4779bc9143c643728ba2eef507618
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 11:23:23 +0100

    blogId

--- publish/actions/blogs/get_all_posts.ts
+++ publish/actions/blogs/get_all_posts.ts
@@ -1,15 +1,20 @@
+import { useBlogStore } from "@/stores/blogStore";
 import { useTokenStore } from "@/stores/tokenStore";
 import axios from "axios";
 
 const useGetAllPosts = () => {
   const { token } = useTokenStore();
+  const { blogId } = useBlogStore();
   const getAllPosts = async () => {
     try {
-      const response = await axios.get("/api/blogs/posts/get-all", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.get(
+        `/api/blogs/posts/get-all?blogId=${blogId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       return response;
     } catch (error: any) {
       return error.response;
