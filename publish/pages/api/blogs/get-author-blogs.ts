3fe6ce5565e537d4d2bcdf419824b165b70c847b
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 09:35:31 +0100

    ___

---
+++ publish/pages/api/blogs/get-author-blogs.ts
@@ -0,0 +1,32 @@
+import { allowMethods } from "next-method-guard";
+import Blog from "@/schema/Blog";
+import { NextApiRequest, NextApiResponse } from "next";
+import DecodedToken from "@/interfaces/DecodedToken";
+import decodeToken from "@/utils/decode_token";
+import Author from "@/schema/Author";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.headers.authorization) {
+    res.status(401).json({
+      message: "Unauthorized request",
+    });
+    return;
+  }
+  const token = req.headers.authorization.split(" ")[1];
+
+  const decoded: DecodedToken | null = decodeToken(token as string);
+
+  if (!decoded) {
+    res.status(400).send("Token expired");
+    return;
+  }
+
+  const author = await Author.findOne({ email: decoded.email });
+
+  if (!author) {
+    res.status(404).json({ message: "No account associated with this email" });
+    return;
+  }
+};
+
+export default allowMethods(["GET"])(handler);
