e21d153cab1cd65acab79f294ef4540010eb9194
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 17:07:13 +0100

    added icon to open post externally

--- publish/components/dashboard/posts/PostCard.tsx
+++ publish/components/dashboard/posts/PostCard.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import styles from "./styles.module.scss";
 import { CgOptions } from "react-icons/cg";
 import { useTheme } from "@/contexts/ThemeContext";
+import { HiOutlineExternalLink } from "react-icons/hi";
 
 const PostCard = ({ title }: { title: string }) => {
   const { theme } = useTheme();
   return (
     <div className={styles.post_card_container}>
-      <h1>{title}</h1>
+      <h1 className="flex">
+        {title}
+        <span className="text-sm ml-1">
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <HiOutlineExternalLink />
+          </a>
+        </span>
+      </h1>
       <div className={styles.side}>
         <p className="">539 views</p>
         <button data-theme={theme}>
