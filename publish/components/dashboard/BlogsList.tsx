70218a5d6d21ac96ffec629a9407f5e2e4b969b5
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-23 09:53:57 +0100

    useEffect

--- publish/components/dashboard/BlogsList.tsx
+++ publish/components/dashboard/BlogsList.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import getAllBlogs from "@/actions/blogs/get_all_blogs";
+import { useTokenStore } from "@/stores/tokenStore";
+import { useEffect } from "react";
 
 const BlogsList = () => {
+  const { token } = useTokenStore();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getAllBlogs(token);
+      console.log(res);
+    })();
+  }, []);
+
   return <div>BlogsList</div>;
 };
 
