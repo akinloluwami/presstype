3660b81597c6b945b2b8e7284dcf1931fe3d2664
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-26 15:31:14 +0100

    ___

--- publish/pages/api/auth/signupwithmagiclink.ts
+++ publish/pages/api/auth/signupwithmagiclink.ts
@@ -48,13 +48,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const magicLinkUrl = `${process.env.BASE_URL}/api/auth/callback/signup?token=${accessToken}&email=${email}`;
     const to = email;
     const subject = "Welcome to PressType";
-    const html = `
+    const body = `
     <h1>Welcome to PressType</h1>
     <p>Click the link below to verify your email and complete your signup</p>
     <a href="http://${magicLinkUrl}" target="_blank" style="background-color: #007bff; color: #fff; padding: 12px 24px; border-radius: 4px; text-decoration: none;">Complete signup</a>
     `;
 
-    await sendMail(to, subject, html);
+    await sendMail({ to, subject, body });
     res.status(201).json({
       message: "Sign up successful, check your email to complete your signup.",
     });
