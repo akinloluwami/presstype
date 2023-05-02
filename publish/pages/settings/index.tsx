8f4a2187053a993895aa8f3e0eb2502eadad3972
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-02 11:02:46 +0100

    changed styling to use tailwind classes

--- publish/pages/settings/index.tsx
+++ publish/pages/settings/index.tsx
@@ -8,24 +8,32 @@ const Index = () => {
   const [expanded, setExpanded] = useState<boolean>(false);
   return (
     <DashboardLayout page_name="Settings">
-      <div className={styles.settings_card}>
-        <h4>Publication settings</h4>
-        <div className={styles.settings_card_container} data-theme={theme}>
-          <div className={styles.settings_card_container_header}>
+      <div>
+        <h1 className="text-2xl mb-3">Publication settings</h1>
+        <div className="bg-secondary_dark p-5 rounded-md">
+          <div className="flex items-center justify-between">
             <div className="">
-              <h5>Title & Description</h5>
+              <h5 className="font-semibold text-lg">Title & Description</h5>
               <small>
                 The details used to identify your publication around the web
               </small>
             </div>
-            <button onClick={() => setExpanded(!expanded)}>Expand</button>
           </div>
-          <div
-            className={`${styles.settings_card_content} ${
-              expanded && styles.active
-            } `}
-          >
-            <input type="text" />
+          <div className={""}>
+            {expanded && (
+              <>
+                <input
+                  type="text"
+                  className="settings-input"
+                  placeholder="Title"
+                />
+                <textarea
+                  placeholder="Description"
+                  className="settings-textarea"
+                />
+                <button className="settings-btn mt-3">Save</button>
+              </>
+            )}
           </div>
         </div>
       </div>
