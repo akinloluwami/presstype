6c88d535af3603aa77291647404707ff1177a9fb
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-02 21:21:50 +0100

    base

--- publish/pages/api/blogs/update-logo.ts
+++ publish/pages/api/blogs/update-logo.ts
@@ -1,1 +1,25 @@
-const handler = () => {};
+import { NextApiRequest, NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { fileUrl, blogId } = req.body;
+
+  if (!fileUrl) {
+    res.status(400).json({
+      message: "File Url  is required",
+    });
+    return;
+  }
+
+  if (!blogId) {
+    res.status(400).json({
+      message: "Blog Id is required",
+    });
+    return;
+  }
+
+  try {
+  } catch (error) {}
+};
+
+export default allowMethods(["PUT"])(handler);
