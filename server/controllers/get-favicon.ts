60c7c6f548159d74f0d66e7336472af0abb6128d
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-04 13:27:50 +0100

    get favicon

--- server/controllers/get-favicon.ts
+++ server/controllers/get-favicon.ts
@@ -1,9 +1,26 @@
 import type { Request, Response } from "express";
+import Blog from "../schema/Blog";
 
 const getFavicon = async (req: Request, res: Response) => {
   const { blog } = req.query;
   try {
-  } catch (error) {}
+    const blogExist = await Blog.findOne({ subdomain: blog });
+
+    if (!blogExist) {
+      res.status(404).json({
+        message: `${blog} not found`,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      favicon: blogExist.blog_logo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 };
 
 export default getFavicon;
