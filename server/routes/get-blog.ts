55931c928693c4f25388dcc66ad4c75ced0cae0c
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-04 13:30:04 +0100

    favicon

--- server/routes/get-blog.ts
+++ server/routes/get-blog.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import getBlog from "../controllers/get-blog";
+import getFavicon from "../controllers/get-favicon";
 
 const router = Router();
 
 router.get("/", getBlog);
+router.get("/favicon", getFavicon);
 
 export default router;
