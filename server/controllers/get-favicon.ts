3e390789933880b85b65c6b30a4d604d350dab99
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-04 13:25:24 +0100

    get favicon function

---
+++ server/controllers/get-favicon.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from "express";
+
+const getFavicon = async (req: Request, res: Response) => {
+  const { blog } = req.query;
+  try {
+  } catch (error) {}
+};
+
+export default getFavicon;
